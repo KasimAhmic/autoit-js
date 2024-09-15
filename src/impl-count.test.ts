730e8b545d6f7f7f5d9bc8c49d3c6cf5ca8274bc
Kasim Ahmic <kasim.ahmic@gmail.com>
2024-09-15 00:10:04 -0400

    test: Add unit test for tracking implementation progress

---
+++ src/impl-count.test.ts
@@ -0,0 +1,40 @@
+import { AutoIt } from './autoit';
+
+describe('AutoIt JS', () => {
+  it('should have every AutoIt method mapped', () => {
+    const au3 = new AutoIt();
+
+    const methods = Object.getOwnPropertyNames(AutoIt.prototype)
+      .filter((method) => method !== 'constructor')
+      .filter((method) => method[0] === method[0].toUpperCase());
+
+    vi.spyOn(au3, 'load').mockImplementation(function () {
+      // @ts-expect-error - Mocking a private property
+      this.lib = {
+        func: () => () => true,
+      };
+    });
+
+    au3.load();
+
+    let report = '';
+
+    for (const method of methods) {
+      try {
+        // @ts-expect-error - This is a dynamic call
+        au3[method]();
+
+        report += `- [x] ${method}\n`;
+      } catch (error) {
+        report += `- [ ] ${method}\n`;
+      }
+    }
+
+    const implementedCount = report.match(/- \[x\]/g)?.length ?? 0;
+
+    console.log('Progress:', implementedCount, '/', methods.length);
+    console.log(report);
+
+    expect(implementedCount).toBe(methods.length);
+  });
+});
