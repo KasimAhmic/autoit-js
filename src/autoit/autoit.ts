706b5ee203c0192d2a4b4473c65c7ce6dd83b9f7
Kasim Ahmic <kasim.ahmic@gmail.com>
2024-09-16 20:57:27 -0400

    feat: Implement ControlEnableByHandle

--- src/autoit/autoit.ts
+++ src/autoit/autoit.ts
@@ -243,9 +243,13 @@ export class AutoIt {
     );
   }
 
-  // TODO: Implement
   ControlEnableByHandle(hWnd: THWND, hCtrl: THWND): number {
-    throw new Error('Unimplemented');
+    return this.invoke(
+      'AU3_ControlEnableByHandle',
+      DataType.Int32,
+      [DataType.UInt64, DataType.UInt64],
+      [hWnd, hCtrl],
+    );
   }
 
   // TODO: Implement
