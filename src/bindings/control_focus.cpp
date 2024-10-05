93ccf370e9836d1d9daf195f2ef8b380d62b4fa2
Kasim Ahmic <kasim.ahmic@gmail.com>
2024-10-04 22:02:09 -0400

    feat: Implement ControlFocus

--- src/bindings/control_focus.cpp
+++ src/bindings/control_focus.cpp
@@ -6,10 +6,17 @@
 Napi::Object ControlFocus(const Napi::CallbackInfo &info) {
     const Napi::Env env = info.Env();
 
-    // TODO: Handle validation
-    if (const auto [success, error] = ValidateArguments(info, 0, {}); !success) {
+    if (const auto [success, error] = ValidateArguments(info, 3, {napi_string, napi_string, napi_string}); !success) {
         ReturnResult(false, StatusCode::ValidationError, error);
     }
+
+    const std::u16string title = info[0].As<Napi::String>().Utf16Value();
+    const std::u16string text = info[1].As<Napi::String>().Utf16Value();
+    const std::u16string control = info[2].As<Napi::String>().Utf16Value();
+
+    const auto *szTitle = reinterpret_cast<const wchar_t *>(title.c_str());
+    const auto *szText = reinterpret_cast<const wchar_t *>(text.c_str());
+    const auto *szControl = reinterpret_cast<const wchar_t *>(control.c_str());
 
     const auto [ControlFocus, errorCode, error] = LoadAutoItFunction(AU3_ControlFocus);
 
@@ -17,8 +24,7 @@ Napi::Object ControlFocus(const Napi::CallbackInfo &info) {
         ReturnResult(false, errorCode, error);
     }
 
-    // TODO: Call the function
-    // ControlFocus();
+    const int code = ControlFocus(szTitle, szText, szControl);
 
-    ReturnResult(true, StatusCode::None, env.Null());
+    ReturnResult(code == 1, code, env.Null());
 }
