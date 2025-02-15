afbaf80bbfac8e3815b40542d1dc32b543fd2615
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:12:03 -0500

    fix: Fix return type

--- src/win-get-pos-by-handle.ts
+++ src/win-get-pos-by-handle.ts
@@ -1,13 +1,13 @@
 import koffi from 'koffi';
 
 import { IRect, LPRECT, Rect } from './@types/rect';
-import { HWND, VOID } from './@types/win32';
+import { HWND, INT } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
 export function WinGetPosByHandle(windowHandle: number): IRect {
   const rect = new Rect();
 
-  autoit.invoke('AU3_WinGetPosByHandle', VOID, [HWND, koffi.out(LPRECT)], [windowHandle, rect]);
+  autoit.invoke('AU3_WinGetPosByHandle', INT, [HWND, koffi.out(LPRECT)], [windowHandle, rect]);
 
   return rect;
 }
