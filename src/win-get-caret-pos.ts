b5b372f6951e4c90fe12a758bd9aca8e0971ae2a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:11:13 -0500

    fix: Fix return type

--- src/win-get-caret-pos.ts
+++ src/win-get-caret-pos.ts
@@ -1,13 +1,13 @@
 import koffi from 'koffi';
 
 import { IPoint, LPPOINT, Point } from './@types/point';
-import { VOID } from './@types/win32';
+import { INT } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
 export function WinGetCaretPos(): IPoint {
   const point = new Point();
 
-  autoit.invoke('AU3_WinGetCaretPos', VOID, [koffi.out(LPPOINT)], [point]);
+  autoit.invoke('AU3_WinGetCaretPos', INT, [koffi.out(LPPOINT)], [point]);
 
   return point;
 }
