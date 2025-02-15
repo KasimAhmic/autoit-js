f77399ffcd273f90bbf342c4fdffc64740d0b409
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:11:23 -0500

    refactor: Rename characters to characterCount

--- src/win-get-class-list-by-handle.ts
+++ src/win-get-class-list-by-handle.ts
@@ -2,8 +2,8 @@ import { HWND, INT, LPWSTR, VOID } from './@types/win32';
 import { autoit } from './autoit/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
-export function WinGetClassListByHandle(windowHandle: number, characters: number = 1024): string {
-  const [buffer, length] = createUnicodeBuffer(characters);
+export function WinGetClassListByHandle(windowHandle: number, characterCount: number = 1024): string {
+  const [buffer, length] = createUnicodeBuffer(characterCount);
 
   autoit.invoke('AU3_WinGetClassListByHandle', VOID, [HWND, LPWSTR, INT], [windowHandle, buffer, length]);
 
