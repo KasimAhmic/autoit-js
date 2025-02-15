7ea49cf1101edb91e31c304dea753273d4c62d1b
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:08:05 -0500

    refactor: Rename arguments

--- src/control-get-handle.ts
+++ src/control-get-handle.ts
@@ -1,6 +1,6 @@
 import { INT, LPCWSTR } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
-export function ControlGetHandle(windowHandle: number, controlName: string): number {
-  return autoit.invoke('AU3_ControlGetHandle', INT, [INT, LPCWSTR], [windowHandle, controlName]);
+export function ControlGetHandle(windowHandle: number, control: string): number {
+  return autoit.invoke('AU3_ControlGetHandle', INT, [INT, LPCWSTR], [windowHandle, control]);
 }
