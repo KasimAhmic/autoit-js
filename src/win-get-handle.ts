c82748faf7fb38893c665b4e07a543599377219a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:11:59 -0500

    fix: Use empty string as default instead of null for text argument

--- src/win-get-handle.ts
+++ src/win-get-handle.ts
@@ -1,6 +1,6 @@
 import { INT, LPCWSTR } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
-export function WinGetHandle(title: string, text: string | null = null): number {
+export function WinGetHandle(title: string, text: string = ''): number {
   return autoit.invoke('AU3_WinGetHandle', INT, [LPCWSTR, LPCWSTR], [title, text]);
 }
