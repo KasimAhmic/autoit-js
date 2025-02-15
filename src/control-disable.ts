7d1a42fca792f98604a3e3c60556c48d197690a6
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:07:56 -0500

    refactor: Rename arguments

--- src/control-disable.ts
+++ src/control-disable.ts
@@ -1,6 +1,6 @@
 import { INT, LPCWSTR } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
-export function ControlDisable(szTitle: string, szText: string, szControl: string): number {
-  return autoit.invoke('AU3_ControlDisable', INT, [LPCWSTR, LPCWSTR, LPCWSTR], [szTitle, szText, szControl]);
+export function ControlDisable(title: string, text: string, control: string): number {
+  return autoit.invoke('AU3_ControlDisable', INT, [LPCWSTR, LPCWSTR, LPCWSTR], [title, text, control]);
 }
