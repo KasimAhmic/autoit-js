c6318436bab7ac7aa529710c843d53fe01668db1
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:12:34 -0500

    fix: Fix return type

--- src/win-get-process.ts
+++ src/win-get-process.ts
@@ -1,6 +1,6 @@
-import { INT, LPCWSTR } from './@types/win32';
+import { DWORD, LPCWSTR } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
 export function WinGetProcess(title: string, text: string = ''): number {
-  return autoit.invoke('AU3_WinGetProcess', INT, [LPCWSTR, LPCWSTR], [title, text]);
+  return autoit.invoke('AU3_WinGetProcess', DWORD, [LPCWSTR, LPCWSTR], [title, text]);
 }
