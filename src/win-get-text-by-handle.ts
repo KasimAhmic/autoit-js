2a8eeeeff4823485bda0e15da12f89055aac1ade
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:12:40 -0500

    refactor: Rename characters to characterCount

--- src/win-get-text-by-handle.ts
+++ src/win-get-text-by-handle.ts
@@ -2,8 +2,8 @@ import { INT, LPWSTR, VOID } from './@types/win32';
 import { autoit } from './autoit/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
-export function WinGetTextByHandle(windowHandle: number, characters: number = 1024): string {
-  const [buffer, length] = createUnicodeBuffer(characters);
+export function WinGetTextByHandle(windowHandle: number, characterCount: number = 1024): string {
+  const [buffer, length] = createUnicodeBuffer(characterCount);
 
   autoit.invoke('AU3_WinGetTextByHandle', VOID, [INT, LPWSTR, INT], [windowHandle, buffer, length]);
 
