908975fe43eb22b64ab6813bac51af68f6d6f733
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:12:55 -0500

    refactor: Rename characters to characterCount

--- src/win-get-title.ts
+++ src/win-get-title.ts
@@ -2,8 +2,8 @@ import { INT, LPCWSTR, LPWSTR, VOID } from './@types/win32';
 import { autoit } from './autoit/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
-export function WinGetTitle(title: string, text: string = '', characters: number = 1024): string {
-  const [buffer, length] = createUnicodeBuffer(characters);
+export function WinGetTitle(title: string, text: string = '', characterCount: number = 1024): string {
+  const [buffer, length] = createUnicodeBuffer(characterCount);
 
   autoit.invoke('AU3_WinGetTitle', VOID, [LPCWSTR, LPCWSTR, LPWSTR, INT], [title, text, buffer, length]);
 
