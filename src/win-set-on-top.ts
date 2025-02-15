38470c6740a441a73cd3ba654067c502ea5ebba4
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:13:18 -0500

    refactor: Rename WinSetOnTopFlag enum

--- src/win-set-on-top.ts
+++ src/win-set-on-top.ts
@@ -1,11 +1,11 @@
 import { INT, LPCWSTR } from './@types/win32';
 import { autoit } from './autoit/autoit';
 
-export enum WinSetOnTopFlag {
-  NoTopMost = 0,
-  TopMost = 1,
+export enum OnTop {
+  Yes = 0,
+  No = 1,
 }
 
-export function WinSetOnTop(title: string, text: string = '', flag: WinSetOnTopFlag): number {
+export function WinSetOnTop(title: string, text: string = '', flag: OnTop): number {
   return autoit.invoke('AU3_WinSetOnTop', INT, [LPCWSTR, LPCWSTR, INT], [title, text, flag]);
 }
