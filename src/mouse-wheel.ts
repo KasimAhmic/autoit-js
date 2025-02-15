63b2c2b5cab74b69e99bfb34cbb161756672167c
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:10:40 -0500

    feat: Add default for clicks

--- src/mouse-wheel.ts
+++ src/mouse-wheel.ts
@@ -6,6 +6,6 @@ export enum ScrollDirection {
   Down = 'down',
 }
 
-export function MouseWheel(direction: ScrollDirection, clicks: number): void {
+export function MouseWheel(direction: ScrollDirection, clicks: number = 1): void {
   autoit.invoke('AU3_MouseWheel', VOID, [LPCWSTR, INT], [direction, clicks]);
 }
