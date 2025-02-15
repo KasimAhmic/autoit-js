4e9ed9c773664f561dbf666a72f9a5cc2d74fed3
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-02-15 14:11:06 -0500

    fix: Fix return type

--- src/statusbar-get-text-by-handle.ts
+++ src/statusbar-get-text-by-handle.ts
@@ -1,17 +1,17 @@
-import { HWND, INT, LPWSTR, VOID } from './@types/win32';
+import { HWND, INT, LPWSTR } from './@types/win32';
 import { autoit } from './autoit/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
 export function StatusbarGetTextByHandle(
   windowHandle: number,
   part: number = 1,
-  characters: number = 1024,
+  characterCount: number = 1024,
 ): string {
-  const [buffer, length] = createUnicodeBuffer(characters);
+  const [buffer, length] = createUnicodeBuffer(characterCount);
 
   autoit.invoke(
     'AU3_StatusbarGetTextByHandle',
-    VOID,
+    INT,
     [HWND, INT, LPWSTR, INT],
     [windowHandle, part, buffer, length],
   );
