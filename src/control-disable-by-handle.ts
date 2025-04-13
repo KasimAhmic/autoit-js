0defaeb54a7b5a88b44432517ebf14b8c0939da6
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-13 14:09:55 -0400

    docs: Add docs for ControlDisable

--- src/control-disable-by-handle.ts
+++ src/control-disable-by-handle.ts
@@ -1,6 +1,25 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Disables a control in a window. Searches for the window and control by the handle of each.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to disable.
+ *
+ * @returns 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlDisableByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * // Disable the Notepad window's edit control.
+ * ControlDisableByHandle(windowHandle, controlHandle);
+ * ```
+ */
 export function ControlDisableByHandle(windowHandle: bigint, controlHandle: bigint): number {
   return autoit.invoke('AU3_ControlDisableByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
 }
--- src/control-disable.ts
+++ src/control-disable.ts
@@ -1,6 +1,23 @@
 import { INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Disables a control in a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Text found in the window to access.
+ * @param controlId The control to disable.
+ *
+ * @returns @returns 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlDisable } from '@ahmic/autoit-js';
+ *
+ * // Disable the Notepad window's edit control.
+ * ControlDisable('Untitled - Notepad', 'Edit1');
+ * ```
+ */
 export function ControlDisable(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
     'AU3_ControlDisable',
