8d3ce6fc2f29b8aa78922a037c2ae18e70d2f9ed
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-13 14:13:22 -0400

    docs: Add docs for ControlEnable

--- src/control-enable-by-handle.ts
+++ src/control-enable-by-handle.ts
@@ -1,6 +1,30 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Enables a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to enable.
+ *
+ * @returns 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlDisableByHandle, ControlEnableByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * // Disable the Notepad window's edit control.
+ * ControlDisableByHandle(windowHandle, controlHandle);
+ *
+ * // Enable the Notepad window's edit control.
+ * ControlEnableByHandle(windowHandle, controlHandle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlEnable.htm
+ */
 export function ControlEnableByHandle(windowHandle: bigint, controlHandle: bigint): number {
   return autoit.invoke('AU3_ControlEnableByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
 }
--- src/control-enable.ts
+++ src/control-enable.ts
@@ -1,6 +1,28 @@
 import { INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Enables a control in a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Text found in the window to access.
+ * @param controlId The control to enable.
+ *
+ * @returns 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlEnable, ControlDisable } from '@ahmic/autoit-js';
+ *
+ * // Disable the Notepad window's edit control.
+ * ControlDisable('Untitled - Notepad', 'Edit1');
+ *
+ * // Enable the Notepad window's edit control.
+ * ControlEnable('Untitled - Notepad', 'Edit1');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlEnable.htm
+ */
 export function ControlEnable(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
     'AU3_ControlEnable',
