f7aa78de265242aad53caa72251b69e35e18ecaa
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-15 14:34:09 -0400

    docs: Add docs for ControlGetHandle

--- src/control-get-handle-as-text.ts
+++ src/control-get-handle-as-text.ts
@@ -3,6 +3,27 @@ import { autoit } from './lib/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 import { HWND_HEX_SIZE } from './util/constants';
 
+/**
+ * Gets the handle of a control in a window as a hexadecimal string. Searches for the control by the title of
+ * its window, the text found in the window, and the ID of the control.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Text found in the window to access.
+ * @param controlId The control to get the handle for.
+ *
+ * @returns The handle of the control as a hexadecimal string.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetHandleAsText } from '@ahmic/autoit-js';
+ *
+ * const handle = ControlGetHandleAsText('Untitled - Notepad', '', 'Edit1');
+ *
+ * console.log(handle); // Output: "0x0000000000000001"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetHandle.htm
+ */
 export function ControlGetHandleAsText(
   windowTitle: string,
   windowText: string = '',
--- src/control-get-handle.ts
+++ src/control-get-handle.ts
@@ -3,6 +3,27 @@ import koffi from 'koffi';
 import { HWND, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Gets the handle of a control in a window. Searches for the control by the handle of its window, and the ID
+ * of the control.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlId The control to get the handle of.
+ *
+ * @returns The handle of the control.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * console.log(controlHandle); // Output: 0x0000000000000001
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetHandle.htm
+ */
 export function ControlGetHandle(windowHandle: bigint, controlId: string): bigint {
   const handleRef = autoit.invoke('AU3_ControlGetHandle', HWND, [HWND, LPCWSTR], [windowHandle, controlId]);
 
