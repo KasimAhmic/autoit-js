7692ac04e9ce086b7cc48fd9b1767795e6ecfae9
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-15 14:45:15 -0400

    docs: Add docs for Rect

--- src/@types/rect.ts
+++ src/@types/rect.ts
@@ -3,9 +3,24 @@ import koffi from 'koffi';
 import { LONG } from './win32';
 
 export interface IRect {
+  /**
+   * The left coordinate of the rectangle. Can be used as the X coordinate of the window/control.
+   */
   left: number;
+
+  /**
+   * The top coordinate of the rectangle. Can be used as the Y coordinate of the window/control.
+   */
   top: number;
+
+  /**
+   * The right coordinate of the rectangle. Can be used as the width of the window/control.
+   */
   right: number;
+
+  /**
+   * The bottom coordinate of the rectangle. Can be used as the height of the window/control.
+   */
   bottom: number;
 }
 
@@ -15,11 +30,18 @@ export class Rect implements IRect {
   right: number;
   bottom: number;
 
-  constructor(options?: IRect) {
-    this.left = options?.left ?? 0;
-    this.top = options?.top ?? 0;
-    this.right = options?.right ?? 0;
-    this.bottom = options?.bottom ?? 0;
+  /**
+   * Creates a new Rect object with the specified coordinates. Serves as a `RECT` struct compatible object.
+   *
+   * @param rect The coordinates of the rectangle. All properties default to 0.
+   *
+   * @see https://learn.microsoft.com/en-us/windows/win32/api/windef/ns-windef-rect
+   */
+  constructor(rect?: Partial<IRect>) {
+    this.left = rect?.left ?? 0;
+    this.top = rect?.top ?? 0;
+    this.right = rect?.right ?? 0;
+    this.bottom = rect?.bottom ?? 0;
   }
 }
 
