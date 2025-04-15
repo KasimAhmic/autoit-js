dfbc50d6c4ef63dbec8a43b38f77f3bc51048f7b
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-15 15:08:55 -0400

    docs: Add docs for WinGetState

--- src/win-get-state.ts
+++ src/win-get-state.ts
@@ -11,14 +11,59 @@ export enum WindowProperty {
 }
 
 export type WindowState = {
+  /**
+   * Indicates if the window exists.
+   */
   exists: boolean;
+
+  /**
+   * Indicates if the window is visible.
+   */
   visible: boolean;
+
+  /**
+   * Indicates if the window is enabled.
+   */
   enabled: boolean;
+
+  /**
+   * Indicates if the window is active.
+   */
   active: boolean;
+
+  /**
+   * Indicates if the window is minimized.
+   */
   minimized: boolean;
+
+  /**
+   * Indicates if the window is maximized.
+   */
   maximized: boolean;
 };
 
+/**
+ * Returns the state of a window. Searches for the window by its title, and the text found within the window.
+ * Though the original AutoIt function returns a bitmask, this function returns an object with boolean
+ * properties for each state for ease of use.
+ *
+ * @param windowTitle The title of the window to check.
+ * @param windowText The text of the window to check.
+ *
+ * @returns An object containing the state of the window.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetState } from '@ahmic/autoit-js';
+ *
+ * const state = WinGetState('Untitled - Notepad');
+ *
+ * console.log(state.exists); // true if the window exists
+ * console.log(state.visible); // true if the window is visible
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/func/WinGetState.htm
+ */
 export function WinGetState(windowTitle: string, windowText: string = ''): WindowState {
   const state = autoit.invoke('AU3_WinGetState', INT, [LPCWSTR, LPCWSTR], [windowTitle, windowText]);
 
