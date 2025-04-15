cefe17602978d59289be7ff8f2ecc07648f5c57a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-15 14:34:33 -0400

    docs: Add docs for ControlFocus

--- src/control-focus.ts
+++ src/control-focus.ts
@@ -1,6 +1,25 @@
 import { INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Sets the focus to a control in a window. Searches for the control by the title of its window, the text
+ * found within its window, and the ID of the control.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText The text found in the window to access.
+ * @param controlId The ID of the control to focus.
+ *
+ * @return 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlFocus } from '@ahmic/autoit-js';
+ *
+ * ControlFocus('Untitled - Notepad', '', 'Edit1');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlFocus.htm
+ */
 export function ControlFocus(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
     'AU3_ControlFocus',
