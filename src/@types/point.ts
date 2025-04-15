88d76013a34c811d0f6e733937fc7b96dda8f0ac
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-15 14:45:27 -0400

    docs: Add docs for Point

--- src/@types/point.ts
+++ src/@types/point.ts
@@ -3,7 +3,14 @@ import koffi from 'koffi';
 import { LONG } from './win32';
 
 export interface IPoint {
+  /**
+   * The X coordinate of the point.
+   */
   x: number;
+
+  /**
+   * The Y coordinate of the point.
+   */
   y: number;
 }
 
@@ -11,9 +18,16 @@ export class Point implements IPoint {
   x: number;
   y: number;
 
-  constructor(options?: IPoint) {
-    this.x = options?.x ?? 0;
-    this.y = options?.y ?? 0;
+  /**
+   * Creates a new Point object with the specified coordinates. Serves as a `POINT` struct compatible object.
+   *
+   * @param point The coordinates of the point. All properties default to 0.
+   *
+   * @see https://learn.microsoft.com/en-us/windows/win32/api/windef/ns-windef-point
+   */
+  constructor(point?: IPoint) {
+    this.x = point?.x ?? 0;
+    this.y = point?.y ?? 0;
   }
 }
 
