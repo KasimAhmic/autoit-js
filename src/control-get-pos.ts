d4d56414da31742fa16d6ff313be291b8d096a79
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-15 14:33:53 -0400

    docs: Add docs for ControlGetPos

--- src/control-get-pos.ts
+++ src/control-get-pos.ts
@@ -1,9 +1,30 @@
 import koffi from 'koffi';
 
-import { INT, IRect, LPCWSTR, LPRECT, Rect } from './@types';
+import { INT, LPCWSTR, LPRECT, Rect } from './@types';
 import { autoit } from './lib/autoit';
 
-export function ControlGetPos(windowTitle: string, windowText: string, controlId: string): IRect {
+/**
+ * Gets the position of a control in a window, relative to the window itself. Searches for the control by the
+ * title of its window, the text found within its window, and the ID of the control.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Text found in the window to access.
+ * @param controlId The control to get the position for.
+ *
+ * @returns The position of the control as a {@link Rect} object.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetPos } from '@ahmic/autoit-js';
+ *
+ * const rect = ControlGetPos('Untitled - Notepad', '', 'Edit1');
+ *
+ * console.log(rect); // Output: { left: 0, top: 0, right: 100, bottom: 100 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetPos.htm
+ */
+export function ControlGetPos(windowTitle: string, windowText: string, controlId: string): Rect {
   const rect = new Rect();
 
   autoit.invoke(
