232139840949e4fc789675fe280207a6a4b96a3a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:39:25 -0400

    docs: Add WinActivate docs

--- src/win-activate-by-handle.ts
+++ src/win-activate-by-handle.ts
@@ -1,6 +1,23 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Activates a window.
+ *
+ * @param windowHandle The handle of the window to activate.
+ *
+ * @returns 1 if successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinActivateByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const handle = WinGetHandle('Untitled - Notepad');
+ * WinActivateByHandle(handle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinActivate.htm
+ */
 export function WinActivateByHandle(windowHandle: bigint): number {
   return autoit.invoke('AU3_WinActivateByHandle', INT, [HWND], [windowHandle]);
 }
