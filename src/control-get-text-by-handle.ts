a2f4f9df2958ba8900d41ed7e54acefe06771761
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 19:32:25 -0400

    docs: Add ControlGetText docs

--- src/control-get-text-by-handle.ts
+++ src/control-get-text-by-handle.ts
@@ -2,6 +2,29 @@ import { HWND, INT, LPWSTR, VOID } from './@types';
 import { autoit } from './lib/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
+/**
+ * Retrieves the text from a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to retrieve text from.
+ * @param characterCount The maximum number of characters to retrieve. Default is 1024.
+ *
+ * @returns The text of the control.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetTextByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * const text = ControlGetTextByHandle(windowHandle, controlHandle);
+ *
+ * console.log(text); // Output: "Sample text"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetText.htm
+ */
 export function ControlGetTextByHandle(
   windowHandle: bigint,
   controlHandle: bigint,
