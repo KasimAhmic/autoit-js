4f88cb5b0e74217d13ac030e9df2cafd1c9e7860
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:35:38 -0400

    docs: Add ControlSetText docs

--- src/control-set-text-by-handle.ts
+++ src/control-set-text-by-handle.ts
@@ -1,6 +1,25 @@
 import { HWND, INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Sets the text of a control in a window. Searches for the control by its handle.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to set text for.
+ * @param value The text to set for the control.
+ *
+ * @returns 1 if successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { ControlSetTextByHandle, ControlGetHandle } from '@ahmic/autoit-js';
+ *
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * ControlSetTextByHandle(windowHandle, controlHandle, 'Hello, World!');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlSetText.htm
+ */
 export function ControlSetTextByHandle(windowHandle: bigint, controlHandle: bigint, value: string): number {
   return autoit.invoke(
     'AU3_ControlSetTextByHandle',
--- src/control-set-text.ts
+++ src/control-set-text.ts
@@ -1,6 +1,26 @@
 import { INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Sets the text of a control in a window. Searches for the control by the title of its window, the text
+ * found within its window, and the ID of the control.
+ *
+ * @param windowTitle The title of the window to search for.
+ * @param windowText Optional text found in the window.
+ * @param controlId The ID of the control to set text for.
+ * @param value The text to set for the control.
+ *
+ * @returns 1 if successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { ControlSetText } from '@ahmic/autoit-js';
+ *
+ * ControlSetText('Untitled - Notepad', '', 'Edit1', 'Hello, World!');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlSetText.htm
+ */
 export function ControlSetText(
   windowTitle: string,
   windowText: string,
