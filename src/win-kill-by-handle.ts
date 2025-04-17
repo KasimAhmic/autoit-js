b7602293cbb59fc25e101980992f8ec189083082
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:43:07 -0400

    docs: Add WinKill docs

--- src/win-kill-by-handle.ts
+++ src/win-kill-by-handle.ts
@@ -1,6 +1,23 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Forces a window to close.
+ *
+ * @param windowHandle The handle of the window to close.
+ *
+ * @returns 1 if successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinKillByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const handle = WinGetHandle('Untitled - Notepad');
+ * WinKillByHandle(handle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinKill.htm
+ */
 export function WinKillByHandle(windowHandle: bigint): number {
   return autoit.invoke('AU3_WinKillByHandle', INT, [HWND], [windowHandle]);
 }
