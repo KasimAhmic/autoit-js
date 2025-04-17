73246b51fedcdf83e30ea0f814b2a729e6c77c14
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:39:47 -0400

    docs: Add WinClose docs

--- src/win-close-by-handle.ts
+++ src/win-close-by-handle.ts
@@ -1,6 +1,23 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Closes a window.
+ *
+ * @param windowHandle The handle of the window to close.
+ *
+ * @returns 1 if successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinCloseByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const handle = WinGetHandle('Untitled - Notepad');
+ * WinCloseByHandle(handle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinClose.htm
+ */
 export function WinCloseByHandle(windowHandle: bigint): number {
   return autoit.invoke('AU3_WinCloseByHandle', INT, [HWND], [windowHandle]);
 }
