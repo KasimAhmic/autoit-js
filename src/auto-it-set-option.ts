6beb473545207f66c4946dabf5378b4ef0acc47c
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 19:29:07 -0400

    docs: Update AutoItSetOption docs

--- src/auto-it-set-option.ts
+++ src/auto-it-set-option.ts
@@ -3,15 +3,22 @@ import { autoit } from './lib/autoit';
 import { AutoItOption } from './opt';
 
 /**
- * Changes the operation of various AutoIt functions/parameters. Note that this function and {@linkcode Opt}
- * are essentially aliases of one another and you can use them interchangeably.
+ * Changes the operation of various AutoIt functions/parameters. This function can be used interchangeably
+ * with {@linkcode Opt}.
  *
  * @param option The option to change. See {@linkcode AutoItOption} for details.
  * @param value The value to assign to the option. It varies depending on the option being set.
  *
  * @returns The previous setting of the option.
  *
- * @see https://www.autoitscript.com/autoit3/docs/functions/Opt.htm
+ * @example
+ * ```typescript
+ * import { AutoItSetOption, AutoItOption } from '@ahmic/autoit-js';
+ *
+ * AutoItSetOption(AutoItOption.AutoItWinTitleMatchMode, 2);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/AutoItSetOption.htm
  */
 export function AutoItSetOption(option: AutoItOption, value: number): number {
   return autoit.invoke('AU3_AutoItSetOption', INT, [LPCWSTR, INT], [option, value]);
