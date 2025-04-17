c84dcb5c45227b86a01cd6430811ee2d51c56369
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:42:45 -0400

    docs: Add WinGetTitle docs

--- src/win-get-title.ts
+++ src/win-get-title.ts
@@ -2,6 +2,24 @@ import { INT, LPCWSTR, LPWSTR, VOID } from './@types';
 import { autoit } from './lib/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
+/**
+ * Retrieves the title of a window.
+ *
+ * @param windowTitle The title of the window to search for.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns The title of the window if found, or an empty string if not found.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetTitle } from '@ahmic/autoit-js';
+ *
+ * const title = WinGetTitle('Untitled - Notepad');
+ * console.log(title); // Output: "Untitled - Notepad"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetTitle.htm
+ */
 export function WinGetTitle(
   windowTitle: string,
   windowText: string = '',
