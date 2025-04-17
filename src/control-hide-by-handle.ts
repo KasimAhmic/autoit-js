63bf4c66b334f20a93ad2812d924ca8a1369744b
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:35:11 -0400

    docs: Add ControlHide docs

--- src/control-hide-by-handle.ts
+++ src/control-hide-by-handle.ts
@@ -1,6 +1,26 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Hides a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to hide.
+ *
+ * @returns 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlHideByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * ControlHideByHandle(windowHandle, controlHandle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlHide.htm
+ */
 export function ControlHideByHandle(windowHandle: bigint, controlHandle: bigint): number {
   return autoit.invoke('AU3_ControlHideByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
 }
