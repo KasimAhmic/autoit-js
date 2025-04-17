5045923438580e9f8985e41735c83fcdd3edd774
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-16 21:38:02 -0400

    docs: Add ControlShow docs

--- src/control-show-by-handle.ts
+++ src/control-show-by-handle.ts
@@ -1,6 +1,26 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Shows a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to show.
+ *
+ * @returns 1 if success, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { ControlShowByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * ControlShowByHandle(windowHandle, controlHandle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlShow.htm
+ */
 export function ControlShowByHandle(windowHandle: bigint, controlHandle: bigint): number {
   return autoit.invoke('AU3_ControlShowByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
 }
