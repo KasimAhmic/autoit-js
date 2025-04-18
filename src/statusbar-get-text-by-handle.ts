306b820ace6e849acf05d51b8046d3328333a298
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 20:48:24 -0400

    docs: Add StatusbarGetText docs

--- src/statusbar-get-text-by-handle.ts
+++ src/statusbar-get-text-by-handle.ts
@@ -2,6 +2,38 @@ import { HWND, INT, LPWSTR } from './@types';
 import { autoit } from './lib/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
+/**
+ * Retrieves the text from a specified part of a status bar in a window. Note that the status bar parts use
+ * 1-based indexing.
+ *
+ * @param windowHandle The handle of the window containing the status bar.
+ * @param part The part of the status bar to retrieve text from (default is 1).
+ * @param characterCount The maximum number of characters to retrieve (default is 1024).
+ *
+ * @returns The text of the specified part of the status bar.
+ *
+ * @example
+ * ```typescript
+ * import { StatusbarGetText, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ *
+ * // The first part of Notepad's status bar is empty
+ * const empty = StatusbarGetText(windowHandle, 1);
+ * const position = StatusbarGetText(windowHandle, 2);
+ * const zoom = StatusbarGetText(windowHandle, 3);
+ * const lingEndings = StatusbarGetText(windowHandle, 4);
+ * const encoding = StatusbarGetText(windowHandle, 5);
+ *
+ * console.log(empty) // ""
+ * console.log(position) // "  Ln 1, Col 1"
+ * console.log(zoom) // " 100%"
+ * console.log(lingEndings) // " Windows (CRLF)"
+ * console.log(encoding) // " UTF-8"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/StatusbarGetText.htm
+ */
 export function StatusbarGetTextByHandle(
   windowHandle: bigint,
   part: number = 1,
--- src/statusbar-get-text.ts
+++ src/statusbar-get-text.ts
@@ -2,6 +2,37 @@ import { INT, LPCWSTR, LPWSTR } from './@types';
 import { autoit } from './lib/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 
+/**
+ * Retrieves the text from a specified part of a status bar in a window. Note that the status bar parts use
+ * 1-based indexing.
+ *
+ * @param windowTitle The title of the window containing the status bar.
+ * @param windowText Optional text found in the window.
+ * @param part The part of the status bar to retrieve text from (default is 1).
+ * @param characterCount The maximum number of characters to retrieve (default is 1024).
+ *
+ * @returns The text of the specified part of the status bar.
+ *
+ * @example
+ * ```typescript
+ * import { StatusbarGetText } from '@ahmic/autoit-js';
+ *
+ * // The first part of Notepad's status bar is empty
+ * const empty = StatusbarGetText('Untitled - Notepad', '', 1);
+ * const position = StatusbarGetText('Untitled - Notepad', '', 2);
+ * const zoom = StatusbarGetText('Untitled - Notepad', '', 3);
+ * const lingEndings = StatusbarGetText('Untitled - Notepad', '', 4);
+ * const encoding = StatusbarGetText('Untitled - Notepad', '', 5);
+ *
+ * console.log(empty) // ""
+ * console.log(position) // "  Ln 1, Col 1"
+ * console.log(zoom) // " 100%"
+ * console.log(lingEndings) // " Windows (CRLF)"
+ * console.log(encoding) // " UTF-8"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/StatusbarGetText.htm
+ */
 export function StatusbarGetText(
   windowTitle: string,
   windowText: string = '',
