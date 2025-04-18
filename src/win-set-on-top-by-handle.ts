1755778f294e08cbeca633e37cf8e79ba9ffdfd1
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:42:36 -0400

    docs: Add WinSetOnTop docs

--- src/win-set-on-top-by-handle.ts
+++ src/win-set-on-top-by-handle.ts
@@ -1,7 +1,25 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
-import { OnTop } from './win-set-on-top';
 
-export function WinSetOnTopByHandle(windowHandle: bigint, flag: OnTop): number {
-  return autoit.invoke('AU3_WinSetOnTopByHandle', INT, [HWND, INT], [windowHandle, flag]);
+/**
+ * Sets whether a window is on top of all other windows.
+ *
+ * @param windowHandle The handle of the window to set on top.
+ * @param onTop Whether to set the window on top or not.
+ *
+ * @returns 1 if the operation was successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinSetOnTopByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ *
+ * WinSetOnTopByHandle(windowHandle, true);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinSetOnTop.htm
+ */
+export function WinSetOnTopByHandle(windowHandle: bigint, onTop: boolean): number {
+  return autoit.invoke('AU3_WinSetOnTopByHandle', INT, [HWND, INT], [windowHandle, onTop ? 1 : 0]);
 }
--- src/win-set-on-top.ts
+++ src/win-set-on-top.ts
@@ -1,11 +1,29 @@
 import { INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
-export enum OnTop {
-  Yes = 0,
-  No = 1,
-}
-
-export function WinSetOnTop(windowTitle: string, windowText: string = '', flag: OnTop): number {
-  return autoit.invoke('AU3_WinSetOnTop', INT, [LPCWSTR, LPCWSTR, INT], [windowTitle, windowText, flag]);
+/**
+ * Sets whether a window is on top of all other windows.
+ *
+ * @param windowTitle The title of the window to set on top.
+ * @param windowText Optional text found in the window.
+ * @param onTop Whether to set the window on top or not.
+ *
+ * @returns 1 if the operation was successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinSetOnTop } from '@ahmic/autoit-js';
+ *
+ * WinSetOnTop('Untitled - Notepad', '', true);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinSetOnTop.htm
+ */
+export function WinSetOnTop(windowTitle: string, windowText: string = '', onTop: boolean): number {
+  return autoit.invoke(
+    'AU3_WinSetOnTop',
+    INT,
+    [LPCWSTR, LPCWSTR, INT],
+    [windowTitle, windowText, onTop ? 1 : 0],
+  );
 }
