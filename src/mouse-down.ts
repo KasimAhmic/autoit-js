754c7bcd4ccc2953056fdc7350ed6ed56dd88a6c
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:39:33 -0400

    docs: Add MouseDown docs

--- src/mouse-down.ts
+++ src/mouse-down.ts
@@ -2,6 +2,20 @@ import { LPCWSTR, VOID } from './@types';
 import { autoit } from './lib/autoit';
 import { MouseButton } from './mouse-click';
 
+/**
+ * Simulates holding down a mouse button.
+ *
+ * @param button The mouse button to hold down. See {@linkcode MouseButton} for details.
+ *
+ * @example
+ * ```typescript
+ * import { MouseDown } from '@ahmic/autoit-js';
+ *
+ * MouseDown('left');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/MouseDown.htm
+ */
 export function MouseDown(button: MouseButton = MouseButton.Left): void {
   return autoit.invoke('AU3_MouseDown', VOID, [LPCWSTR], [button]);
 }
