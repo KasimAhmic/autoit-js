e238e7870a95dea95d8b9e59232329ff14765066
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:40:58 -0400

    docs: Add WinActive docs

--- src/win-active-by-handle.ts
+++ src/win-active-by-handle.ts
@@ -1,6 +1,25 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Checks if a window is currently active.
+ *
+ * @param windowHandle The handle of the window to check.
+ *
+ * @returns 1 if the window is active, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinActiveByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const isActive = WinActiveByHandle(windowHandle);
+ *
+ * console.log(isActive); // Output: 1
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinActive.htm
+ */
 export function WinActiveByHandle(windowHandle: bigint): number {
   return autoit.invoke('AU3_WinActiveByHandle', INT, [HWND], [windowHandle]);
 }
