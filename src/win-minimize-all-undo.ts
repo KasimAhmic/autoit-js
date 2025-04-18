9dd8318c64fca33d98e03272c9c391943fb0b6da
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 20:55:14 -0400

    docs: Add WinMinimizeAllUndo docs

--- src/win-minimize-all-undo.ts
+++ src/win-minimize-all-undo.ts
@@ -1,6 +1,18 @@
 import { VOID } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Undoes the minimize all action, restoring all minimized windows.
+ *
+ * @example
+ * ```typescript
+ * import { WinMinimizeAllUndo } from '@ahmic/autoit-js';
+ *
+ * WinMinimizeAllUndo();
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinMinimizeAllUndo.htm
+ */
 export function WinMinimizeAllUndo(): void {
   autoit.invoke('AU3_WinMinimizeAllUndo', VOID, [], []);
 }
