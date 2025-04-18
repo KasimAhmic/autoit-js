1b941406315f69483d0bd79b2114c5374e9474ea
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:41:33 -0400

    docs: Add WinGetHandle docs

--- src/win-get-handle-as-text.ts
+++ src/win-get-handle-as-text.ts
@@ -3,6 +3,25 @@ import { autoit } from './lib/autoit';
 import { createUnicodeBuffer, unicodeBufferToString } from './util';
 import { HWND_HEX_SIZE } from './util/constants';
 
+/**
+ * Retrieves the handle of a window as a string.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns The handle of the window as a string.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetHandleAsText } from '@ahmic/autoit-js';
+ *
+ * const handleText = WinGetHandleAsText('Untitled - Notepad');
+ *
+ * console.log(handleText); // Output: "0x00123456" (example output)
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetHandle.htm
+ */
 export function WinGetHandleAsText(windowTitle: string, windowText: string = ''): string {
   const [buffer, length] = createUnicodeBuffer(HWND_HEX_SIZE);
 
--- src/win-get-handle.ts
+++ src/win-get-handle.ts
@@ -3,6 +3,25 @@ import koffi from 'koffi';
 import { HWND, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Retrieves the handle of a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns The handle of the window as a bigint.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const handle = WinGetHandle('Untitled - Notepad');
+ *
+ * console.log(handle); // Output: 123456n
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetHandle.htm
+ */
 export function WinGetHandle(windowTitle: string, windowText: string = ''): bigint {
   const handleRef = autoit.invoke('AU3_WinGetHandle', HWND, [LPCWSTR, LPCWSTR], [windowTitle, windowText]);
 
