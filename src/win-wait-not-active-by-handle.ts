4eaa3847e8b1c6c3dac891046de508770e310acb
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:43:40 -0400

    docs: Add WinWaitNotActive docs

--- src/win-wait-not-active-by-handle.ts
+++ src/win-wait-not-active-by-handle.ts
@@ -1,6 +1,25 @@
 import { HWND, INT } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Waits for a window to become inactive.
+ *
+ * @param windowHandle The handle of the window to wait for.
+ * @param timeout The timeout in seconds. Defaults to 0 (wait indefinitely).
+ *
+ * @returns 1 if the window becomes inactive, 0 if the timeout is reached.
+ *
+ * @example
+ * ```typescript
+ * import { WinWaitNotActiveByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ *
+ * WinWaitNotActiveByHandle(windowHandle, 10);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinWaitNotActive.htm
+ */
 export function WinWaitNotActiveByHandle(windowHandle: bigint, timeout: number = 0): number {
   return autoit.invoke('AU3_WinWaitNotActiveByHandle', INT, [HWND, INT], [windowHandle, timeout]);
 }
