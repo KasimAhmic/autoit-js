7a01982fb496845db16f483612ef91745f9efd50
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 20:54:20 -0400

    docs: Add WinMenuSelectItem docs

--- src/win-menu-select-item-by-handle.ts
+++ src/win-menu-select-item-by-handle.ts
@@ -1,6 +1,35 @@
 import { HWND, INT, LPCWSTR } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Selects a menu item in a window.
+ *
+ * Underlined items in the menu must be selected using the `&` prefix. For example, if you're following the
+ * sequence `File -> Open`, you would use `&File` and `&Open` in the parameters.
+ *
+ * @param windowHandle The handle of the window containing the menu.
+ * @param item1 The text of the first-level menu item.
+ * @param item2 Optional text of the second-level menu item.
+ * @param item3 Optional text of the third-level menu item.
+ * @param item4 Optional text of the fourth-level menu item.
+ * @param item5 Optional text of the fifth-level menu item.
+ * @param item6 Optional text of the sixth-level menu item.
+ * @param item7 Optional text of the seventh-level menu item.
+ * @param item8 Optional text of the eighth-level menu item.
+ *
+ * @returns 1 if successful, 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinMenuSelectItemByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ *
+ * WinMenuSelectItemByHandle(windowHandle, 'File', 'Open');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinMenuSelectItem.htm
+ */
 export function WinMenuSelectItemByHandle(
   windowHandle: bigint,
   item1: string,
