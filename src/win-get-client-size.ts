8aa47c6670946999ec2ca75955a37661498a6c7e
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:41:22 -0400

    docs: Add WinGetClientSize docs

--- src/win-get-client-size.ts
+++ src/win-get-client-size.ts
@@ -3,6 +3,25 @@ import koffi from 'koffi';
 import { INT, IRect, LPCWSTR, LPRECT, Rect } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Retrieves the client area dimensions of a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns An object containing the width and height of the client area.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetClientSize } from '@ahmic/autoit-js';
+ *
+ * const clientSize = WinGetClientSize('Untitled - Notepad');
+ *
+ * console.log(clientSize); // Output: { width: 800, height: 600 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetClientSize.htm
+ */
 export function WinGetClientSize(windowTitle: string, windowText: string = ''): IRect {
   const rect = new Rect();
 
