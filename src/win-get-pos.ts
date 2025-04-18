d5271c9672f95a26cb8108e8a49a29ac9eddd603
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 20:50:08 -0400

    docs: Add WinGetPost docs

--- src/win-get-pos.ts
+++ src/win-get-pos.ts
@@ -3,6 +3,25 @@ import koffi from 'koffi';
 import { INT, IRect, LPCWSTR, LPRECT, Rect } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Retrieves the position and size of a window.
+ *
+ * @param windowTitle The title of the window.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns The position and size of the window as a {@linkcode IRect} object.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetPos } from '@ahmic/autoit-js';
+ *
+ * const rect = WinGetPos('Untitled - Notepad');
+ *
+ * console.log(rect); // Output: { left: 100, top: 100, right: 200, bottom: 200 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetPos.htm
+ */
 export function WinGetPos(windowTitle: string, windowText: string = ''): IRect {
   const rect = new Rect();
 
