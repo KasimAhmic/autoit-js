841ca1b5c1484e79a1991dba2e70e684aeed3cb4
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-04-17 23:41:48 -0400

    docs: Add WinGetProcess docs

--- src/win-get-process-by-handle.ts
+++ src/win-get-process-by-handle.ts
@@ -1,6 +1,25 @@
 import { DWORD, HWND } from './@types';
 import { autoit } from './lib/autoit';
 
+/**
+ * Retrieves the process ID associated with a window handle.
+ *
+ * @param windowHandle The handle of the window to access.
+ *
+ * @returns The process ID as a number.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetProcessByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const processId = WinGetProcessByHandle(windowHandle);
+ *
+ * console.log(processId); // Output: 1234
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetProcess.htm
+ */
 export function WinGetProcessByHandle(windowHandle: bigint): number {
   return autoit.invoke('AU3_WinGetProcessByHandle', DWORD, [HWND], [windowHandle]);
 }
