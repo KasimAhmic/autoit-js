bebda9c4fcde0c97fbd1bd508e6ddd6ba4a27328
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-07-31 15:37:50 -0400

    feat: Implement async variant of ClipPut
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ClipPut to ClipPutSync
    
    #10

--- src/clip-put.ts
+++ src/clip-put.ts
@@ -8,6 +8,26 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
+ * import { ClipPutSync, ClipGetSync } from '@ahmic/autoit-js';
+ *
+ * ClipPutSync('Hello');
+ *
+ * console.log(ClipGetSync()); // Outputs: Hello
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ClipPut.htm
+ */
+export function ClipPutSync(value: string): void {
+  return autoit.invoke('AU3_ClipPut', VOID, [LPCWSTR], [value]);
+}
+
+/**
+ * Writes text to the clipboard.
+ *
+ * @param value The text to put in the clipboard.
+ *
+ * @example
+ * ```typescript
  * import { ClipPut, ClipGet } from '@ahmic/autoit-js';
  *
  * ClipPut('Hello');
@@ -17,6 +37,6 @@ import { autoit } from './lib/autoit';
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ClipPut.htm
  */
-export function ClipPut(value: string): void {
-  return autoit.invoke('AU3_ClipPut', VOID, [LPCWSTR], [value]);
+export function ClipPut(value: string): Promise<void> {
+  return autoit.invokeAsync('AU3_ClipPut', VOID, [LPCWSTR], [value]);
 }
