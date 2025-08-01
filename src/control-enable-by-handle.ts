c73c8308104b64fe1c08c7ff7811b18628896f75
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 11:29:05 -0400

    feat: Implement async variant of ControlEnableByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlEnableByHandle to ControlEnableByHandleSync
    
    #10

--- src/control-enable-by-handle.ts
+++ src/control-enable-by-handle.ts
@@ -11,16 +11,54 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlDisableByHandle, ControlEnableByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import {
+ *   ControlDisableByHandleSync,
+ *   ControlEnableByHandleSync,
+ *   ControlGetHandleSync,
+ *   WinGetHandleSync,
+ * } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
  *
  * // Disable the Notepad window's edit control.
- * ControlDisableByHandle(windowHandle, controlHandle);
+ * ControlDisableByHandleSync(windowHandle, controlHandle);
  *
  * // Enable the Notepad window's edit control.
- * ControlEnableByHandle(windowHandle, controlHandle);
+ * ControlEnableByHandleSync(windowHandle, controlHandle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlEnable.htm
+ */
+export function ControlEnableByHandleSync(windowHandle: bigint, controlHandle: bigint): number {
+  return autoit.invoke('AU3_ControlEnableByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
+}
+
+/**
+ * Enables a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to enable.
+ *
+ * @returns A promise that resolves to 1 if success, or 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import {
+ *   ControlDisableByHandle,
+ *   ControlEnableByHandle,
+ *   ControlGetHandle,
+ *   WinGetHandle,
+ * } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * // Disable the Notepad window's edit control.
+ * await ControlDisableByHandle(windowHandle, controlHandle);
+ *
+ * // Enable the Notepad window's edit control.
+ * await ControlEnableByHandle(windowHandle, controlHandle);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlEnable.htm
