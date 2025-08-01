f633c7601713d319ebcf9cc83d8f3e50e321835a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 16:56:45 -0400

    feat: Implement async variant of ControlMove
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlMove to ControlMoveSync
    
    #10

--- src/control-move.ts
+++ src/control-move.ts
@@ -16,9 +16,49 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
+ * import { ControlMoveSync } from '@ahmic/autoit-js';
+ *
+ * ControlMoveSync('Untitled - Notepad', '', 'Edit1', 100, 100, 200, 50);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlMove.htm
+ */
+export function ControlMoveSync(
+  windowTitle: string,
+  windowText: string,
+  controlId: string,
+  x: number,
+  y: number,
+  width: number = -1,
+  height: number = -1,
+): number {
+  // TODO: Add an overload that allows someone to pass a Rect object
+  return autoit.invoke(
+    'AU3_ControlMove',
+    INT,
+    [LPCWSTR, LPCWSTR, LPCWSTR, INT, INT, INT, INT],
+    [windowTitle, windowText, controlId, x, y, width, height],
+  );
+}
+
+/**
+ * Moves and resizes a control in a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ * @param controlId The ID of the control to move or resize.
+ * @param x The new X coordinate of the control.
+ * @param y The new Y coordinate of the control.
+ * @param width The new width of the control. Default is -1 (no change).
+ * @param height The new height of the control. Default is -1 (no change).
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
  * import { ControlMove } from '@ahmic/autoit-js';
  *
- * ControlMove('Untitled - Notepad', '', 'Edit1', 100, 100, 200, 50);
+ * await ControlMove('Untitled - Notepad', '', 'Edit1', 100, 100, 200, 50);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlMove.htm
@@ -31,9 +71,9 @@ export function ControlMove(
   y: number,
   width: number = -1,
   height: number = -1,
-): number {
+): Promise<number> {
   // TODO: Add an overload that allows someone to pass a Rect object
-  return autoit.invoke(
+  return autoit.invokeAsync(
     'AU3_ControlMove',
     INT,
     [LPCWSTR, LPCWSTR, LPCWSTR, INT, INT, INT, INT],
