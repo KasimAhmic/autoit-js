8930b56b7c2e06e701e96877dd49953b00f34101
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 12:03:47 -0400

    feat: Implement async variant of ControlGetText
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlGetText to ControlGetTextSync
    
    #10

--- src/control-get-text.ts
+++ src/control-get-text.ts
@@ -14,16 +14,16 @@ import { createUnicodeBuffer, unicodeBufferToString } from './util';
  *
  * @example
  * ```typescript
- * import { ControlGetText } from '@ahmic/autoit-js';
+ * import { ControlGetTextSync } from '@ahmic/autoit-js';
  *
- * const text = ControlGetText('Untitled - Notepad', '', 'Edit1');
+ * const text = ControlGetTextSync('Untitled - Notepad', '', 'Edit1');
  *
  * console.log(text); // Output: "Sample text"
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetText.htm
  */
-export function ControlGetText(
+export function ControlGetTextSync(
   windowTitle: string,
   windowText: string,
   controlId: string,
@@ -32,6 +32,45 @@ export function ControlGetText(
   const [buffer, length] = createUnicodeBuffer(characterCount);
 
   autoit.invoke(
+    'AU3_ControlGetText',
+    VOID,
+    [LPCWSTR, LPCWSTR, LPCWSTR, LPWSTR, INT],
+    [windowTitle, windowText, controlId, buffer, length],
+  );
+
+  return unicodeBufferToString(buffer);
+}
+
+/**
+ * Retrieves the text from a control in a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ * @param controlId The ID of the control to retrieve text from.
+ * @param characterCount The maximum number of characters to retrieve. Default is 1024.
+ *
+ * @returns A promise that resolves to the text of the control.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetText } from '@ahmic/autoit-js';
+ *
+ * const text = await ControlGetText('Untitled - Notepad', '', 'Edit1');
+ *
+ * console.log(text); // Output: "Sample text"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetText.htm
+ */
+export async function ControlGetText(
+  windowTitle: string,
+  windowText: string,
+  controlId: string,
+  characterCount: number = 1024,
+): Promise<string> {
+  const [buffer, length] = createUnicodeBuffer(characterCount);
+
+  await autoit.invokeAsync(
     'AU3_ControlGetText',
     VOID,
     [LPCWSTR, LPCWSTR, LPCWSTR, LPWSTR, INT],
