8bfa87f632f4249ecfb7d645076c5ebdba95ab9d
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 16:58:42 -0400

    feat: Implement async variant of ControlMoveByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlMoveByHandle to ControlMoveByHandleSync
    
    #10

--- src/control-move-by-handle.ts
+++ src/control-move-by-handle.ts
@@ -15,12 +15,52 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
+ * import { ControlMoveByHandleSync, ControlGetHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
+ *
+ * ControlMoveByHandleSync(windowHandle, controlHandle, 100, 100, 200, 50);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlMove.htm
+ */
+export function ControlMoveByHandleSync(
+  windowHandle: bigint,
+  controlHandle: bigint,
+  x: number,
+  y: number,
+  width: number = -1,
+  height: number = -1,
+): number {
+  return autoit.invoke(
+    'AU3_ControlMoveByHandle',
+    INT,
+    [HWND, HWND, INT, INT, INT, INT],
+    [windowHandle, controlHandle, x, y, width, height],
+  );
+}
+
+/**
+ * Moves and resizes a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to move or resize.
+ * @param x The new X coordinate of the control.
+ * @param y The new Y coordinate of the control.
+ * @param width The new width of the control. Default is -1 (no change).
+ * @param height The new height of the control. Default is -1 (no change).
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
  * import { ControlMoveByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
  *
- * ControlMoveByHandle(windowHandle, controlHandle, 100, 100, 200, 50);
+ * await ControlMoveByHandle(windowHandle, controlHandle, 100, 100, 200, 50);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlMove.htm
@@ -32,8 +72,8 @@ export function ControlMoveByHandle(
   y: number,
   width: number = -1,
   height: number = -1,
-): number {
-  return autoit.invoke(
+): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_ControlMoveByHandle',
     INT,
     [HWND, HWND, INT, INT, INT, INT],
