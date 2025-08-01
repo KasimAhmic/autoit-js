43d9ce02715632da96cf11dadf73ff3a2c968e4a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 11:43:31 -0400

    feat: Implement async variant of ControlGetFocusByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlGetFocusByHandle to ControlGetFocusByHandleSync
    
    #10

--- src/control-get-focus-by-handle.ts
+++ src/control-get-focus-by-handle.ts
@@ -11,21 +11,54 @@ import { createUnicodeBuffer, unicodeBufferToString } from './util';
  *
  * @example
  * ```typescript
- * import { ControlGetFocusByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { ControlGetFocusByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
  *
- * const id = ControlGetFocusByHandle(windowHandle);
+ * const id = ControlGetFocusByHandleSync(windowHandle);
  *
  * console.log(id); // Output: "Edit1"
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetFocus.htm
  */
-export function ControlGetFocusByHandle(windowHandle: bigint): string {
+export function ControlGetFocusByHandleSync(windowHandle: bigint): string {
   const [buffer, length] = createUnicodeBuffer(1024);
 
   autoit.invoke('AU3_ControlGetFocusByHandle', VOID, [HWND, LPWSTR, INT], [windowHandle, buffer, length]);
+
+  return unicodeBufferToString(buffer);
+}
+
+/**
+ * Gets the ID of the control that has keyboard focus in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ *
+ * @returns A promise that resolves to the ID of the control that has keyboard focus.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetFocusByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ *
+ * const id = await ControlGetFocusByHandle(windowHandle);
+ *
+ * console.log(id); // Output: "Edit1"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetFocus.htm
+ */
+export async function ControlGetFocusByHandle(windowHandle: bigint): Promise<string> {
+  const [buffer, length] = createUnicodeBuffer(1024);
+
+  await autoit.invokeAsync(
+    'AU3_ControlGetFocusByHandle',
+    VOID,
+    [HWND, LPWSTR, INT],
+    [windowHandle, buffer, length],
+  );
 
   return unicodeBufferToString(buffer);
 }
