179942402070a304903a37bdf9484c09bc80827a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 11:23:33 -0400

    feat: Implement async variant of ControlEnable
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlEnable to ControlEnableSync
    
    #10

--- src/control-enable.ts
+++ src/control-enable.ts
@@ -12,19 +12,50 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlEnable, ControlDisable } from '@ahmic/autoit-js';
+ * import { ControlEnableSync, ControlDisableSync } from '@ahmic/autoit-js';
  *
  * // Disable the Notepad window's edit control.
- * ControlDisable('Untitled - Notepad', 'Edit1');
+ * ControlDisableSync('Untitled - Notepad', 'Edit1');
  *
  * // Enable the Notepad window's edit control.
- * ControlEnable('Untitled - Notepad', 'Edit1');
+ * ControlEnableSync('Untitled - Notepad', 'Edit1');
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlEnable.htm
  */
-export function ControlEnable(windowTitle: string, windowText: string, controlId: string): number {
+export function ControlEnableSync(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
+    'AU3_ControlEnable',
+    INT,
+    [LPCWSTR, LPCWSTR, LPCWSTR],
+    [windowTitle, windowText, controlId],
+  );
+}
+
+/**
+ * Enables a control in a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ * @param controlId The control to enable.
+ *
+ * @returns A promise that resolves to 1 if success, or 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlEnable, ControlDisable } from '@ahmic/autoit-js';
+ *
+ * // Disable the Notepad window's edit control.
+ * await ControlDisable('Untitled - Notepad', 'Edit1');
+ *
+ * // Enable the Notepad window's edit control.
+ * await ControlEnable('Untitled - Notepad', 'Edit1');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlEnable.htm
+ */
+export function ControlEnable(windowTitle: string, windowText: string, controlId: string): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_ControlEnable',
     INT,
     [LPCWSTR, LPCWSTR, LPCWSTR],
