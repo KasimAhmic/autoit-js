fb814146e97ff25a96958537bfea746039c57cf8
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 12:02:30 -0400

    feat: Implement async variant of ControlGetPosByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlGetPosByHandle to ControlGetPosByHandleSync
    
    #10

--- src/control-get-pos-by-handle.ts
+++ src/control-get-pos-by-handle.ts
@@ -13,22 +13,57 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlGetPosByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { ControlGetPosByHandleSync, ControlGetHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
  *
- * const rect = ControlGetPosByHandle(windowHandle, controlHandle);
+ * const rect = ControlGetPosByHandleSync(windowHandle, controlHandle);
  *
  * console.log(rect); // Output: { left: 10, top: 10, right: 100, bottom: 100 }
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetPos.htm
  */
-export function ControlGetPosByHandle(windowHandle: bigint, controlHandle: bigint): Rect {
+export function ControlGetPosByHandleSync(windowHandle: bigint, controlHandle: bigint): Rect {
   const rect = new Rect();
 
   autoit.invoke(
+    'AU3_ControlGetPosByHandle',
+    INT,
+    [HWND, HWND, koffi.out(LPRECT)],
+    [windowHandle, controlHandle, rect],
+  );
+
+  return rect;
+}
+
+/**
+ * Gets the position of a control in a window, relative to the window itself.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to get the position for.
+ *
+ * @returns A promise that resolves to the position of the control as a {@linkcode Rect} object.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetPosByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * const rect = await ControlGetPosByHandle(windowHandle, controlHandle);
+ *
+ * console.log(rect); // Output: { left: 10, top: 10, right: 100, bottom: 100 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetPos.htm
+ */
+export async function ControlGetPosByHandle(windowHandle: bigint, controlHandle: bigint): Promise<Rect> {
+  const rect = new Rect();
+
+  await autoit.invokeAsync(
     'AU3_ControlGetPosByHandle',
     INT,
     [HWND, HWND, koffi.out(LPRECT)],
