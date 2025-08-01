226359298674eceb316e41dfa5014d93edc3b8be
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 12:08:12 -0400

    feat: Implement async variant of ControlHideByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlHideByHandle to ControlHideByHandleSync
    
    #10

--- src/control-hide-by-handle.ts
+++ src/control-hide-by-handle.ts
@@ -11,16 +11,40 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlHideByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { ControlHideByHandleSync, ControlGetHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
  *
- * ControlHideByHandle(windowHandle, controlHandle);
+ * ControlHideByHandleSync(windowHandle, controlHandle);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlHide.htm
  */
-export function ControlHideByHandle(windowHandle: bigint, controlHandle: bigint): number {
+export function ControlHideByHandleSync(windowHandle: bigint, controlHandle: bigint): number {
   return autoit.invoke('AU3_ControlHideByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
+}
+
+/**
+ * Hides a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to hide.
+ *
+ * @returns A promise that resolves to 1 if success, 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlHideByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * await ControlHideByHandle(windowHandle, controlHandle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlHide.htm
+ */
+export function ControlHideByHandle(windowHandle: bigint, controlHandle: bigint): Promise<number> {
+  return autoit.invokeAsync('AU3_ControlHideByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
 }
