055ffe4105a3c053b5d0c504a040625212a94602
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 11:18:41 -0400

    feat: Implement async variant of ControlDisableByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlDisableByHandle to ControlDisableByHandleSync
    
    #10

--- src/control-disable-by-handle.ts
+++ src/control-disable-by-handle.ts
@@ -11,16 +11,40 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlDisableByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { ControlDisableByHandleSync, ControlGetHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
  *
- * ControlDisableByHandle(windowHandle, controlHandle);
+ * ControlDisableByHandleSync(windowHandle, controlHandle);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlDisable.htm
  */
-export function ControlDisableByHandle(windowHandle: bigint, controlHandle: bigint): number {
+export function ControlDisableByHandleSync(windowHandle: bigint, controlHandle: bigint): number {
   return autoit.invoke('AU3_ControlDisableByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
+}
+
+/**
+ * Disables a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to disable.
+ *
+ * @returns A promise that resolves to 1 if success, or 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlDisableByHandle, ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * await ControlDisableByHandle(windowHandle, controlHandle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlDisable.htm
+ */
+export function ControlDisableByHandle(windowHandle: bigint, controlHandle: bigint): Promise<number> {
+  return autoit.invokeAsync('AU3_ControlDisableByHandle', INT, [HWND, HWND], [windowHandle, controlHandle]);
 }
