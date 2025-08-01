1ba2f707944183bbfddff4e8758259870f61f9d0
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 11:45:18 -0400

    feat: Implement async variant of ControlGetHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlGetHandle to ControlGetHandleSync
    
    #10

--- src/control-get-handle.ts
+++ src/control-get-handle.ts
@@ -13,18 +13,49 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { ControlGetHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
  *
  * console.log(controlHandle); // Output: 0x0000000000000001
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetHandle.htm
  */
-export function ControlGetHandle(windowHandle: bigint, controlId: string): bigint {
+export function ControlGetHandleSync(windowHandle: bigint, controlId: string): bigint {
   const handleRef = autoit.invoke('AU3_ControlGetHandle', HWND, [HWND, LPCWSTR], [windowHandle, controlId]);
+
+  return koffi.address(handleRef);
+}
+
+/**
+ * Gets the handle of a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlId The control to get the handle of.
+ *
+ * @returns A promise that resolves to the handle of the control.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
+ *
+ * console.log(controlHandle); // Output: 0x0000000000000001
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetHandle.htm
+ */
+export async function ControlGetHandle(windowHandle: bigint, controlId: string): Promise<bigint> {
+  const handleRef = await autoit.invokeAsync(
+    'AU3_ControlGetHandle',
+    HWND,
+    [HWND, LPCWSTR],
+    [windowHandle, controlId],
+  );
 
   return koffi.address(handleRef);
 }
