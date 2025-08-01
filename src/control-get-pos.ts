e8e2c1f0329e3284f05d2e5812dc5e6e1398a9f8
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 12:01:28 -0400

    feat: Implement async variant of ControlGetPos
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlGetPos to ControlGetPosSync
    
    #10

--- src/control-get-pos.ts
+++ src/control-get-pos.ts
@@ -14,19 +14,56 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlGetPos } from '@ahmic/autoit-js';
+ * import { ControlGetPosSync } from '@ahmic/autoit-js';
  *
- * const rect = ControlGetPos('Untitled - Notepad', '', 'Edit1');
+ * const rect = ControlGetPosSync('Untitled - Notepad', '', 'Edit1');
  *
  * console.log(rect); // Output: { left: 0, top: 0, right: 100, bottom: 100 }
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetPos.htm
  */
-export function ControlGetPos(windowTitle: string, windowText: string, controlId: string): Rect {
+export function ControlGetPosSync(windowTitle: string, windowText: string, controlId: string): Rect {
   const rect = new Rect();
 
   autoit.invoke(
+    'AU3_ControlGetPos',
+    INT,
+    [LPCWSTR, LPCWSTR, LPCWSTR, koffi.out(LPRECT)],
+    [windowTitle, windowText, controlId, rect],
+  );
+
+  return rect;
+}
+
+/**
+ * Gets the position of a control in a window, relative to the window itself.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ * @param controlId The control to get the position for.
+ *
+ * @returns A promise that resolves to the position of the control as a {@linkcode Rect} object.
+ *
+ * @example
+ * ```typescript
+ * import { ControlGetPos } from '@ahmic/autoit-js';
+ *
+ * const rect = await ControlGetPos('Untitled - Notepad', '', 'Edit1');
+ *
+ * console.log(rect); // Output: { left: 0, top: 0, right: 100, bottom: 100 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlGetPos.htm
+ */
+export async function ControlGetPos(
+  windowTitle: string,
+  windowText: string,
+  controlId: string,
+): Promise<Rect> {
+  const rect = new Rect();
+
+  await autoit.invokeAsync(
     'AU3_ControlGetPos',
     INT,
     [LPCWSTR, LPCWSTR, LPCWSTR, koffi.out(LPRECT)],
