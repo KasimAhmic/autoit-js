8d8ea69e53f4dfc1ee26ecbfbec49ea6cd1382e7
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 11:35:54 -0400

    feat: Implement async variant of ControlFocus
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlFocus to ControlFocusSync
    
    #10

--- src/control-focus.ts
+++ src/control-focus.ts
@@ -12,15 +12,42 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlFocus } from '@ahmic/autoit-js';
+ * import { ControlFocusSync } from '@ahmic/autoit-js';
  *
- * ControlFocus('Untitled - Notepad', '', 'Edit1');
+ * ControlFocusSync('Untitled - Notepad', '', 'Edit1');
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlFocus.htm
  */
-export function ControlFocus(windowTitle: string, windowText: string, controlId: string): number {
+export function ControlFocusSync(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
+    'AU3_ControlFocus',
+    INT,
+    [LPCWSTR, LPCWSTR, LPCWSTR],
+    [windowTitle, windowText, controlId],
+  );
+}
+
+/**
+ * Sets the keyboard focus to a control in a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ * @param controlId The ID of the control to focus.
+ *
+ * @return A promise that resolves to 1 if success, or 0 if failure.
+ *
+ * @example
+ * ```typescript
+ * import { ControlFocus } from '@ahmic/autoit-js';
+ *
+ * await ControlFocus('Untitled - Notepad', '', 'Edit1');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlFocus.htm
+ */
+export function ControlFocus(windowTitle: string, windowText: string, controlId: string): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_ControlFocus',
     INT,
     [LPCWSTR, LPCWSTR, LPCWSTR],
