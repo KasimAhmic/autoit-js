6e46752a19c7ee20b0afaead672c76aa0140422d
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-01 12:07:22 -0400

    feat: Implement async variant of ControlHide
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlHide to ControlHideSync
    
    #10

--- src/control-hide.ts
+++ src/control-hide.ts
@@ -12,15 +12,42 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlHide } from '@ahmic/autoit-js';
+ * import { ControlHideSync } from '@ahmic/autoit-js';
  *
- * ControlHide('Untitled - Notepad', '', 'Edit1');
+ * ControlHideSync('Untitled - Notepad', '', 'Edit1');
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlHide.htm
  */
-export function ControlHide(windowTitle: string, windowText: string, controlId: string): number {
+export function ControlHideSync(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
+    'AU3_ControlHide',
+    INT,
+    [LPCWSTR, LPCWSTR, LPCWSTR],
+    [windowTitle, windowText, controlId],
+  );
+}
+
+/**
+ * Hides a control in a window.
+ *
+ * @param windowTitle The title of the window to search for.
+ * @param windowText Optional text found in the window.
+ * @param controlId The ID of the control to hide.
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { ControlHide } from '@ahmic/autoit-js';
+ *
+ * await ControlHide('Untitled - Notepad', '', 'Edit1');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlHide.htm
+ */
+export function ControlHide(windowTitle: string, windowText: string, controlId: string): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_ControlHide',
     INT,
     [LPCWSTR, LPCWSTR, LPCWSTR],
