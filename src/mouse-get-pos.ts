4cf09bdfead180952c9df5b23a8e0eb0c902e01b
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-04 14:35:29 -0400

    feat: Implement async variant of MouseGetPos
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed MouseGetPos to MouseGetPosSync
    
    #10

--- src/mouse-get-pos.ts
+++ src/mouse-get-pos.ts
@@ -10,19 +10,44 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { MouseGetPos } from '@ahmic/autoit-js';
+ * import { MouseGetPosSync } from '@ahmic/autoit-js';
  *
- * const position = MouseGetPos();
+ * const position = MouseGetPosSync();
  *
  * console.log(position); // Output: { x: 100, y: 200 }
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/MouseGetPos.htm
  */
-export function MouseGetPos(): Point {
+export function MouseGetPosSync(): Point {
   const point = new Point();
 
   autoit.invoke('AU3_MouseGetPos', VOID, [koffi.out(LPPOINT)], [point]);
+
+  return point;
+}
+
+/**
+ * Retrieves the current position of the mouse cursor.
+ *
+ * @returns A promise that resolves to a {@link Point} object containing the x and y coordinates of the mouse
+ * cursor.
+ *
+ * @example
+ * ```typescript
+ * import { MouseGetPos } from '@ahmic/autoit-js';
+ *
+ * const position = await MouseGetPos();
+ *
+ * console.log(position); // Output: { x: 100, y: 200 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/MouseGetPos.htm
+ */
+export async function MouseGetPos(): Promise<Point> {
+  const point = new Point();
+
+  await autoit.invokeAsync('AU3_MouseGetPos', VOID, [koffi.out(LPPOINT)], [point]);
 
   return point;
 }
