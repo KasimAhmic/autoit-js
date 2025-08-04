a714e9922e71158dd17e4d9268676db3287eefb9
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-04 14:08:46 -0400

    feat: Implement async variant of ControlShow
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlShow to ControlShowSync
    
    #10

--- src/control-show.ts
+++ src/control-show.ts
@@ -12,15 +12,42 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ControlShow } from '@ahmic/autoit-js';
+ * import { ControlShowSync } from '@ahmic/autoit-js';
  *
- * ControlShow('Untitled - Notepad', '', 'Edit1');
+ * ControlShowSync('Untitled - Notepad', '', 'Edit1');
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlShow.htm
  */
-export function ControlShow(windowTitle: string, windowText: string, controlId: string): number {
+export function ControlShowSync(windowTitle: string, windowText: string, controlId: string): number {
   return autoit.invoke(
+    'AU3_ControlShow',
+    INT,
+    [LPCWSTR, LPCWSTR, LPCWSTR],
+    [windowTitle, windowText, controlId],
+  );
+}
+
+/**
+ * Shows a control in a window.
+ *
+ * @param windowTitle The title of the window to search for.
+ * @param windowText Optional text found in the window.
+ * @param controlId The ID of the control to show.
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { ControlShow } from '@ahmic/autoit-js';
+ *
+ * await ControlShow('Untitled - Notepad', '', 'Edit1');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlShow.htm
+ */
+export function ControlShow(windowTitle: string, windowText: string, controlId: string): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_ControlShow',
     INT,
     [LPCWSTR, LPCWSTR, LPCWSTR],
