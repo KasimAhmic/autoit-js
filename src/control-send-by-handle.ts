2c7467990cf600dd4d9b707df2fface844ce957a
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-04 14:05:27 -0400

    feat: Implement async variant of ControlSendByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ControlSendByHandle to ControlSendByHandleSync
    
    #10

--- src/control-send-by-handle.ts
+++ src/control-send-by-handle.ts
@@ -14,12 +14,48 @@ import { SendMode } from './send';
  *
  * @example
  * ```typescript
+ * import { ControlGetHandleSync, ControlSendByHandleSync, SendMode, WinGetHandleSync } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const controlHandle = ControlGetHandleSync(windowHandle, 'Edit1');
+ *
+ * ControlSendByHandleSync(windowHandle, controlHandle, 'Hello, World!', SendMode.Default);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ControlSend.htm
+ */
+export function ControlSendByHandleSync(
+  windowHandle: bigint,
+  controlHandle: bigint,
+  value: string,
+  mode: SendMode = SendMode.Default,
+): number {
+  return autoit.invoke(
+    'AU3_ControlSendByHandle',
+    INT,
+    [HWND, HWND, LPCWSTR, INT],
+    [windowHandle, controlHandle, value, mode],
+  );
+}
+
+/**
+ * Sends a string of text to a control in a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ * @param controlHandle The handle of the control to interact with.
+ * @param value The string of text to send.
+ * @param mode The send mode to use. See {@linkcode SendMode} for details. Default is {@linkcode SendMode.Default}.
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
  * import { ControlGetHandle, ControlSendByHandle, SendMode, WinGetHandle } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const controlHandle = ControlGetHandle(windowHandle, 'Edit1');
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const controlHandle = await ControlGetHandle(windowHandle, 'Edit1');
  *
- * ControlSendByHandle(windowHandle, controlHandle, 'Hello, World!', SendMode.Default);
+ * await ControlSendByHandle(windowHandle, controlHandle, 'Hello, World!', SendMode.Default);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ControlSend.htm
@@ -29,8 +65,8 @@ export function ControlSendByHandle(
   controlHandle: bigint,
   value: string,
   mode: SendMode = SendMode.Default,
-): number {
-  return autoit.invoke(
+): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_ControlSendByHandle',
     INT,
     [HWND, HWND, LPCWSTR, INT],
