1c0cdd3b30e5588edfcd50fc6c027790e4ee7727
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-04 14:36:40 -0400

    feat: Implement async variant of MouseMove
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed MouseMove to MouseMoveSync
    
    #10

--- src/mouse-move.ts
+++ src/mouse-move.ts
@@ -12,13 +12,35 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { MouseMove } from '@ahmic/autoit-js';
+ * import { MouseMoveSync } from '@ahmic/autoit-js';
  *
- * MouseMove(100, 200, 50);
+ * MouseMoveSync(100, 200, 50);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/MouseMove.htm
  */
-export function MouseMove(x: number, y: number, speed: number = -1): number {
+export function MouseMoveSync(x: number, y: number, speed: number = -1): number {
   return autoit.invoke('AU3_MouseMove', INT, [INT, INT, INT], [x, y, speed]);
+}
+
+/**
+ * Moves the mouse pointer to a specified location on the screen.
+ *
+ * @param x The x-coordinate to move the mouse to.
+ * @param y The y-coordinate to move the mouse to.
+ * @param speed Optional speed of the mouse movement (1 is slow, 100 is fast, -1 is instant).
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { MouseMove } from '@ahmic/autoit-js';
+ *
+ * await MouseMove(100, 200, 50);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/MouseMove.htm
+ */
+export function MouseMove(x: number, y: number, speed: number = -1): Promise<number> {
+  return autoit.invokeAsync('AU3_MouseMove', INT, [INT, INT, INT], [x, y, speed]);
 }
