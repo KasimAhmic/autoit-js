c84f96ee4ac42ae215b7bfbf862948702d58ad0e
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-05 15:33:33 -0400

    feat: Implement async variant of Sleep
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed Sleep to SleepSync
    
    #10

--- src/sleep.ts
+++ src/sleep.ts
@@ -8,13 +8,31 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { Sleep } from '@ahmic/autoit-js';
+ * import { SleepSync } from '@ahmic/autoit-js';
  *
- * Sleep(1000); // Pauses for 1 second
+ * SleepSync(1000); // Pauses for 1 second
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/Sleep.htm
  */
-export function Sleep(milliseconds: number): void {
+export function SleepSync(milliseconds: number): void {
   return autoit.invoke('AU3_Sleep', VOID, [INT], [milliseconds]);
+}
+
+/**
+ * Pauses the script execution for a specified amount of time.
+ *
+ * @param milliseconds The number of milliseconds to pause.
+ *
+ * @example
+ * ```typescript
+ * import { Sleep } from '@ahmic/autoit-js';
+ *
+ * await Sleep(1000); // Pauses for 1 second
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/Sleep.htm
+ */
+export function Sleep(milliseconds: number): Promise<void> {
+  return autoit.invokeAsync('AU3_Sleep', VOID, [INT], [milliseconds]);
 }
