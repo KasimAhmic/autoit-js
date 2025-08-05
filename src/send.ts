a96e5df79a728f364c0c5b621d14dca80b4bf7cb
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-05 15:31:01 -0400

    feat: Implement async variant of Send
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed Send to SendSync
    
    #10

--- src/send.ts
+++ src/send.ts
@@ -22,13 +22,32 @@ export enum SendMode {
  *
  * @example
  * ```typescript
- * import { Send } from '@ahmic/autoit-js';
+ * import { SendSync } from '@ahmic/autoit-js';
  *
- * Send('Hello, world!');
+ * SendSync('Hello, world!');
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/Send.htm
  */
-export function Send(value: string, mode: SendMode = SendMode.Default): void {
+export function SendSync(value: string, mode: SendMode = SendMode.Default): void {
   return autoit.invoke('AU3_Send', VOID, [LPCWSTR, INT], [value, mode]);
+}
+
+/**
+ * Sends simulated keystrokes to the active window.
+ *
+ * @param value The string of keys to send.
+ * @param mode The mode to use for sending keys. See {@linkcode SendMode} for details.
+ *
+ * @example
+ * ```typescript
+ * import { Send } from '@ahmic/autoit-js';
+ *
+ * await Send('Hello, world!');
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/Send.htm
+ */
+export function Send(value: string, mode: SendMode = SendMode.Default): Promise<void> {
+  return autoit.invokeAsync('AU3_Send', VOID, [LPCWSTR, INT], [value, mode]);
 }
