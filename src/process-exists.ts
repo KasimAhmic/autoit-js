fc692e298956489918779c22d69fa15f422fbc8e
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-05 11:47:27 -0400

    feat: Implement async variant of ProcessExists
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed ProcessExists to ProcessExistsSync
    
    #10

--- src/process-exists.ts
+++ src/process-exists.ts
@@ -10,15 +10,39 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { ProcessExists } from '@ahmic/autoit-js';
+ * import { ProcessExistsSync } from '@ahmic/autoit-js';
  *
- * const exists = ProcessExists('notepad.exe');
+ * const exists = ProcessExistsSync('notepad.exe');
  *
  * console.log(exists); // Output: true
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/ProcessExists.htm
  */
-export function ProcessExists(process: string): boolean {
+export function ProcessExistsSync(process: string): boolean {
   return autoit.invoke('AU3_ProcessExists', INT, [LPCWSTR], [process]) !== 0;
+}
+
+/**
+ * Checks if a process exists.
+ *
+ * @param process The name of the process to check (e.g., 'notepad.exe').
+ *
+ * @returns True if the process exists, false otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { ProcessExists } from '@ahmic/autoit-js';
+ *
+ * const exists = await ProcessExists('notepad.exe');
+ *
+ * console.log(exists); // Output: true
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/ProcessExists.htm
+ */
+export async function ProcessExists(process: string): Promise<boolean> {
+  const processExists = await autoit.invokeAsync('AU3_ProcessExists', INT, [LPCWSTR], [process]);
+
+  return processExists !== 0;
 }
