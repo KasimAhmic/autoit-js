92f1bb29ca5b47aca27dd200b83418c9c270632c
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-05 15:29:08 -0400

    feat: Implement async variant of RunWait
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed RunWait to RunWaitSync
    
    #10

--- src/run-wait.ts
+++ src/run-wait.ts
@@ -13,6 +13,34 @@ import { ShowWindowFlag } from './run';
  *
  * @example
  * ```typescript
+ * import { RunWaitSync } from '@ahmic/autoit-js';
+ *
+ * const exitCode = RunWaitSync('notepad.exe');
+ *
+ * console.log(exitCode); // Output: 0
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/RunWait.htm
+ */
+export function RunWaitSync(
+  program: string,
+  directory: string = '',
+  showFlag: number = ShowWindowFlag.SHOWNORMAL,
+): number {
+  return autoit.invoke('AU3_RunWait', INT, [LPCWSTR, LPCWSTR, INT], [program, directory, showFlag]);
+}
+
+/**
+ * Runs a program and waits for it to close before continuing.
+ *
+ * @param program The name of the program to run.
+ * @param directory Optional working directory for the program.
+ * @param showFlag Optional flag to control how the program's window is shown.
+ *
+ * @returns A promise that resolves to the exit code of the program.
+ *
+ * @example
+ * ```typescript
  * import { RunWait } from '@ahmic/autoit-js';
  *
  * const exitCode = RunWait('notepad.exe');
@@ -26,6 +54,6 @@ export function RunWait(
   program: string,
   directory: string = '',
   showFlag: number = ShowWindowFlag.SHOWNORMAL,
-): number {
-  return autoit.invoke('AU3_RunWait', INT, [LPCWSTR, LPCWSTR, INT], [program, directory, showFlag]);
+): Promise<number> {
+  return autoit.invokeAsync('AU3_RunWait', INT, [LPCWSTR, LPCWSTR, INT], [program, directory, showFlag]);
 }
