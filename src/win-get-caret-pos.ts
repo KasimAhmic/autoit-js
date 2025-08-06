5c6914da5a220e69644864d8a66ee62f6b4c554d
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:01:39 -0400

    feat: Implement async variant of WinGetCaretPos
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinGetCaretPos to WinGetCaretPosSync
    
    #10

--- src/win-get-caret-pos.ts
+++ src/win-get-caret-pos.ts
@@ -1,28 +1,53 @@
 import koffi from 'koffi';
 
-import { INT, LPPOINT, Point } from './@types';
+import { INT, IPoint, LPPOINT, Point } from './@types';
 import { autoit } from './lib/autoit';
 
 /**
  * Retrieves the position of the caret in the active window.
  *
- * @returns An object containing the x and y coordinates of the caret.
+ * @returns An {@linkcode IPoint} object containing the x and y coordinates of the caret.
  *
  * @example
  * ```typescript
- * import { WinGetCaretPos } from '@ahmic/autoit-js';
+ * import { WinGetCaretPosSync } from '@ahmic/autoit-js';
  *
- * const caretPos = WinGetCaretPos();
+ * const caretPos = WinGetCaretPosSync();
  *
  * console.log(caretPos); // Output: { x: 10, y: 20 }
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetCaretPos.htm
  */
-export function WinGetCaretPos(): Point {
+export function WinGetCaretPosSync(): Point {
   const point = new Point();
 
   autoit.invoke('AU3_WinGetCaretPos', INT, [koffi.out(LPPOINT)], [point]);
+
+  return point;
+}
+
+/**
+ * Retrieves the position of the caret in the active window.
+ *
+ * @returns A promise that resolves to an {@linkcode IPoint} object containing the x and y coordinates of the
+ * caret.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetCaretPos } from '@ahmic/autoit-js';
+ *
+ * const caretPos = await WinGetCaretPos();
+ *
+ * console.log(caretPos); // Output: { x: 10, y: 20 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetCaretPos.htm
+ */
+export async function WinGetCaretPos(): Promise<IPoint> {
+  const point = new Point();
+
+  await autoit.invokeAsync('AU3_WinGetCaretPos', INT, [koffi.out(LPPOINT)], [point]);
 
   return point;
 }
