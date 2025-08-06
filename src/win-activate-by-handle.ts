eef4de3b0330197ceaa685c63ee23294a89540bd
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 13:44:21 -0400

    feat: Implement async variant of WinActivateByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinActivateByHandle to WinActivateByHandleSync
    
    #10

--- src/win-activate-by-handle.ts
+++ src/win-activate-by-handle.ts
@@ -10,14 +10,35 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinActivateByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { WinActivateByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const handle = WinGetHandle('Untitled - Notepad');
- * WinActivateByHandle(handle);
+ * const handle = WinGetHandleSync('Untitled - Notepad');
+ * WinActivateByHandleSync(handle);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinActivate.htm
  */
-export function WinActivateByHandle(windowHandle: bigint): number {
+export function WinActivateByHandleSync(windowHandle: bigint): number {
   return autoit.invoke('AU3_WinActivateByHandle', INT, [HWND], [windowHandle]);
+}
+
+/**
+ * Activates a window.
+ *
+ * @param windowHandle The handle of the window to activate.
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinActivateByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const handle = await WinGetHandle('Untitled - Notepad');
+ * await WinActivateByHandle(handle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinActivate.htm
+ */
+export function WinActivateByHandle(windowHandle: bigint): Promise<number> {
+  return autoit.invokeAsync('AU3_WinActivateByHandle', INT, [HWND], [windowHandle]);
 }
