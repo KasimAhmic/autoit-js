3e33d3388f0e02439fbcd8dc87e200701dcae9a2
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 16:34:29 -0400

    feat: Implement async variant of WinWaitByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinWaitByHandle to WinWaitByHandleSync
    
    #10

--- src/win-wait-by-handle.ts
+++ src/win-wait-by-handle.ts
@@ -11,14 +11,36 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinWaitByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { WinWaitByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const handle = WinGetHandle('Untitled - Notepad');
- * WinWaitByHandle(handle, 10);
+ * const handle = WinGetHandleSync('Untitled - Notepad');
+ * WinWaitByHandleSync(handle, 10);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinWait.htm
  */
-export function WinWaitByHandle(windowHandle: bigint, timeout: number = 0): number {
+export function WinWaitByHandleSync(windowHandle: bigint, timeout: number = 0): number {
   return autoit.invoke('AU3_WinWaitByHandle', INT, [HWND, INT], [windowHandle, timeout]);
+}
+
+/**
+ * Waits for a window to exist.
+ *
+ * @param windowHandle The handle of the window to wait for.
+ * @param timeout The timeout in seconds. Default is 0 (wait indefinitely).
+ *
+ * @returns A promise that resolves to 1 if the window exists, or 0 if the timeout is reached.
+ *
+ * @example
+ * ```typescript
+ * import { WinWaitByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const handle = await WinGetHandle('Untitled - Notepad');
+ * await WinWaitByHandle(handle, 10);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinWait.htm
+ */
+export function WinWaitByHandle(windowHandle: bigint, timeout: number = 0): Promise<number> {
+  return autoit.invokeAsync('AU3_WinWaitByHandle', INT, [HWND, INT], [windowHandle, timeout]);
 }
