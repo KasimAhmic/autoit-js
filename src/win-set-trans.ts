64f88aa1a9d0927953a628064ee4d8a1046c1baf
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 16:32:11 -0400

    feat: Implement async variant of WinSetTrans
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinSetTrans to WinSetTransSync
    
    #10

--- src/win-set-trans.ts
+++ src/win-set-trans.ts
@@ -12,15 +12,46 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinSetTrans } from '@ahmic/autoit-js';
+ * import { WinSetTransSync } from '@ahmic/autoit-js';
  *
- * WinSetTrans('Untitled - Notepad', '', 128);
+ * WinSetTransSync('Untitled - Notepad', '', 128);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinSetTrans.htm
  */
-export function WinSetTrans(windowTitle: string, windowText: string = '', transparency: number): number {
+export function WinSetTransSync(windowTitle: string, windowText: string = '', transparency: number): number {
   return autoit.invoke(
+    'AU3_WinSetTrans',
+    INT,
+    [LPCWSTR, LPCWSTR, INT],
+    [windowTitle, windowText, transparency],
+  );
+}
+
+/**
+ * Sets the transparency level of a window. 0 is fully transparent, 255 is fully opaque.
+ *
+ * @param windowTitle The title of the window.
+ * @param windowText Optional text found in the window.
+ * @param transparency The transparency level (0-255).
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinSetTrans } from '@ahmic/autoit-js';
+ *
+ * await WinSetTrans('Untitled - Notepad', '', 128);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinSetTrans.htm
+ */
+export function WinSetTrans(
+  windowTitle: string,
+  windowText: string = '',
+  transparency: number,
+): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_WinSetTrans',
     INT,
     [LPCWSTR, LPCWSTR, INT],
