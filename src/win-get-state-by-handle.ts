edd7f3511c4fb38c88f0d49999d5d136b07f1b1d
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:49:22 -0400

    feat: Implement async variant of WinGetStateByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinGetStateByHandle to WinGetStateByHandleSync
    
    #10

--- src/win-get-state-by-handle.ts
+++ src/win-get-state-by-handle.ts
@@ -10,14 +10,14 @@ import { WindowProperty, WindowState } from './win-get-state';
  *
  * @param windowHandle The handle of the window to check.
  *
- * @returns An object containing the state of the window.
+ * @returns A {@linkcode WindowState} object containing the state of the window.
  *
  * @example
  * ```typescript
- * import { WinGetStateByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { WinGetStateByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const state = WinGetStateByHandle(windowHandle);
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const state = WinGetStateByHandleSync(windowHandle);
  *
  * console.log(state.exists); // true if the window exists
  * console.log(state.visible); // true if the window is visible
@@ -25,8 +25,44 @@ import { WindowProperty, WindowState } from './win-get-state';
  *
  * @see https://www.autoitscript.com/autoit3/docs/func/WinGetState.htm
  */
-export function WinGetStateByHandle(windowHandle: bigint): WindowState {
+export function WinGetStateByHandleSync(windowHandle: bigint): WindowState {
   const state = autoit.invoke('AU3_WinGetStateByHandle', INT, [HWND], [windowHandle]);
+
+  return {
+    exists: (state & WindowProperty.Exists) === WindowProperty.Exists,
+    visible: (state & WindowProperty.Visible) === WindowProperty.Visible,
+    enabled: (state & WindowProperty.Enabled) === WindowProperty.Enabled,
+    active: (state & WindowProperty.Active) === WindowProperty.Active,
+    minimized: (state & WindowProperty.Minimized) === WindowProperty.Minimized,
+    maximized: (state & WindowProperty.Maximized) === WindowProperty.Maximized,
+  };
+}
+
+/**
+ * Returns the state of a window.
+ *
+ * Though the original AutoIt function returns a bitmask, this function returns an object with boolean
+ * properties for each state for ease of use.
+ *
+ * @param windowHandle The handle of the window to check.
+ *
+ * @returns A promise that resolves to a {@linkcode WindowState} object containing the state of the window.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetStateByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const state = await WinGetStateByHandle(windowHandle);
+ *
+ * console.log(state.exists); // true if the window exists
+ * console.log(state.visible); // true if the window is visible
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/func/WinGetState.htm
+ */
+export async function WinGetStateByHandle(windowHandle: bigint): Promise<WindowState> {
+  const state = await autoit.invokeAsync('AU3_WinGetStateByHandle', INT, [HWND], [windowHandle]);
 
   return {
     exists: (state & WindowProperty.Exists) === WindowProperty.Exists,
