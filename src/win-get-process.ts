1e884864dcc511054c9380d151db2d1b1b8c7f70
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:45:51 -0400

    feat: Implement async variant of WinGetProcess
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinGetProcess to WinGetProcessSync
    
    #10

--- src/win-get-process.ts
+++ src/win-get-process.ts
@@ -11,15 +11,38 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinGetProcess } from '@ahmic/autoit-js';
+ * import { WinGetProcessSync } from '@ahmic/autoit-js';
  *
- * const processId = WinGetProcess('Untitled - Notepad');
+ * const processId = WinGetProcessSync('Untitled - Notepad');
  *
  * console.log(processId); // Output: 1234
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetProcess.htm
  */
-export function WinGetProcess(windowTitle: string, windowText: string = ''): number {
+export function WinGetProcessSync(windowTitle: string, windowText: string = ''): number {
   return autoit.invoke('AU3_WinGetProcess', DWORD, [LPCWSTR, LPCWSTR], [windowTitle, windowText]);
+}
+
+/**
+ * Retrieves the process ID associated with a window.
+ *
+ * @param windowTitle The title of the window to access.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns A promise that resolves to the process ID as a number.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetProcess } from '@ahmic/autoit-js';
+ *
+ * const processId = await WinGetProcess('Untitled - Notepad');
+ *
+ * console.log(processId); // Output: 1234
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetProcess.htm
+ */
+export function WinGetProcess(windowTitle: string, windowText: string = ''): Promise<number> {
+  return autoit.invokeAsync('AU3_WinGetProcess', DWORD, [LPCWSTR, LPCWSTR], [windowTitle, windowText]);
 }
