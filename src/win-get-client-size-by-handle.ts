0b35373b687f1d3867b9c91ab6df1937ec62b0f4
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:10:10 -0400

    feat: Implement async variant of WinGetClientSizeByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinGetClientSizeByHandle to WinGetClientSizeByHandleSync
    
    #10

--- src/win-get-client-size-by-handle.ts
+++ src/win-get-client-size-by-handle.ts
@@ -1,6 +1,6 @@
 import koffi from 'koffi';
 
-import { HWND, INT, LPRECT, Rect } from './@types';
+import { HWND, INT, IRect, LPRECT, Rect } from './@types';
 import { autoit } from './lib/autoit';
 
 /**
@@ -8,24 +8,57 @@ import { autoit } from './lib/autoit';
  *
  * @param windowHandle The handle of the window to access.
  *
- * @returns An object containing the width and height of the client area.
+ * @returns An {@linkcode IRect} object containing the width and height of the client area.
  *
  * @example
  * ```typescript
- * import { WinGetClientSizeByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { WinGetClientSizeByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- * const clientSize = WinGetClientSizeByHandle(windowHandle);
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const clientSize = WinGetClientSizeByHandleSync(windowHandle);
  *
  * console.log(clientSize); // Output: { width: 800, height: 600 }
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetClientSize.htm
  */
-export function WinGetClientSizeByHandle(windowHandle: bigint): Rect {
+export function WinGetClientSizeByHandleSync(windowHandle: bigint): Rect {
   const rect = new Rect();
 
   autoit.invoke('AU3_WinGetClientSizeByHandle', INT, [HWND, koffi.out(LPRECT)], [windowHandle, rect]);
+
+  return rect;
+}
+
+/**
+ * Retrieves the client area dimensions of a window.
+ *
+ * @param windowHandle The handle of the window to access.
+ *
+ * @returns A promise that resolves to an {@linkcode IRect} object containing the width and height of the
+ * client area.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetClientSizeByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const clientSize = await WinGetClientSizeByHandle(windowHandle);
+ *
+ * console.log(clientSize); // Output: { width: 800, height: 600 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetClientSize.htm
+ */
+export async function WinGetClientSizeByHandle(windowHandle: bigint): Promise<IRect> {
+  const rect = new Rect();
+
+  await autoit.invokeAsync(
+    'AU3_WinGetClientSizeByHandle',
+    INT,
+    [HWND, koffi.out(LPRECT)],
+    [windowHandle, rect],
+  );
 
   return rect;
 }
