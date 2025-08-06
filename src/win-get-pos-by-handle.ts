79e94067cfe396a2da485b80db7fa1d4fb6f4e75
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:17:11 -0400

    feat: Implement async variant of WinGetPosByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinGetPosByHandle to WinGetPosByHandleSync
    
    #10

--- src/win-get-pos-by-handle.ts
+++ src/win-get-pos-by-handle.ts
@@ -8,25 +8,52 @@ import { autoit } from './lib/autoit';
  *
  * @param windowHandle The handle of the window.
  *
- * @returns The position and size of the window as a {@linkcode IRect} object.
+ * @returns An {@linkcode IRect} object containing the position and size of the window.
  *
  * @example
  * ```typescript
- * import { WinGetHandle, WinGetPosByHandle } from '@ahmic/autoit-js';
+ * import { WinGetHandleSync, WinGetPosByHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
- *
- * const rect = WinGetPosByHandle(windowHandle);
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
+ * const rect = WinGetPosByHandleSync(windowHandle);
  *
  * console.log(rect); // Output: { left: 100, top: 100, right: 200, bottom: 200 }
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetPos.htm
  */
-export function WinGetPosByHandle(windowHandle: bigint): IRect {
+export function WinGetPosByHandleSync(windowHandle: bigint): IRect {
   const rect = new Rect();
 
   autoit.invoke('AU3_WinGetPosByHandle', INT, [HWND, koffi.out(LPRECT)], [windowHandle, rect]);
+
+  return rect;
+}
+
+/**
+ * Retrieves the position and size of a window.
+ *
+ * @param windowHandle The handle of the window.
+ *
+ * @returns A promise that resolves to an {@linkcode IRect} object containing the position and size of the
+ * window.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetHandle, WinGetPosByHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ * const rect = await WinGetPosByHandle(windowHandle);
+ *
+ * console.log(rect); // Output: { left: 100, top: 100, right: 200, bottom: 200 }
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetPos.htm
+ */
+export async function WinGetPosByHandle(windowHandle: bigint): Promise<IRect> {
+  const rect = new Rect();
+
+  await autoit.invokeAsync('AU3_WinGetPosByHandle', INT, [HWND, koffi.out(LPRECT)], [windowHandle, rect]);
 
   return rect;
 }
