849d237b86ff34522c0473a823e106a14d765bca
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:54:23 -0400

    feat: Implement async variant of WinKillByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinKillByHandle to WinKillByHandleSync
    
    #10

--- src/win-kill-by-handle.ts
+++ src/win-kill-by-handle.ts
@@ -10,6 +10,27 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
+ * import { WinKillByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
+ *
+ * const handle = WinGetHandleSync('Untitled - Notepad');
+ * WinKillByHandleSync(handle);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinKill.htm
+ */
+export function WinKillByHandleSync(windowHandle: bigint): number {
+  return autoit.invoke('AU3_WinKillByHandle', INT, [HWND], [windowHandle]);
+}
+
+/**
+ * Forces a window to close.
+ *
+ * @param windowHandle The handle of the window to close.
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
  * import { WinKillByHandle, WinGetHandle } from '@ahmic/autoit-js';
  *
  * const handle = WinGetHandle('Untitled - Notepad');
@@ -18,6 +39,6 @@ import { autoit } from './lib/autoit';
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinKill.htm
  */
-export function WinKillByHandle(windowHandle: bigint): number {
-  return autoit.invoke('AU3_WinKillByHandle', INT, [HWND], [windowHandle]);
+export function WinKillByHandle(windowHandle: bigint): Promise<number> {
+  return autoit.invokeAsync('AU3_WinKillByHandle', INT, [HWND], [windowHandle]);
 }
