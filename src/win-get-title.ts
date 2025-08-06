1b6a4b877803383b29ce5363e8f7714ff3325327
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:53:39 -0400

    feat: Implement async variant of WinGetTitle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinGetTitle to WinGetTitleSync
    
    #10

--- src/win-get-title.ts
+++ src/win-get-title.ts
@@ -12,16 +12,16 @@ import { createUnicodeBuffer, unicodeBufferToString } from './util';
  *
  * @example
  * ```typescript
- * import { WinGetTitle } from '@ahmic/autoit-js';
+ * import { WinGetTitleSync } from '@ahmic/autoit-js';
  *
- * const title = WinGetTitle('Untitled - Notepad');
+ * const title = WinGetTitleSync('Untitled - Notepad');
  *
  * console.log(title); // Output: "Untitled - Notepad"
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetTitle.htm
  */
-export function WinGetTitle(
+export function WinGetTitleSync(
   windowTitle: string,
   windowText: string = '',
   characterCount: number = 1024,
@@ -29,6 +29,42 @@ export function WinGetTitle(
   const [buffer, length] = createUnicodeBuffer(characterCount);
 
   autoit.invoke(
+    'AU3_WinGetTitle',
+    VOID,
+    [LPCWSTR, LPCWSTR, LPWSTR, INT],
+    [windowTitle, windowText, buffer, length],
+  );
+
+  return unicodeBufferToString(buffer);
+}
+
+/**
+ * Retrieves the title of a window.
+ *
+ * @param windowTitle The title of the window to search for.
+ * @param windowText Optional text found in the window.
+ *
+ * @returns A promise that resolves to the title of the window if found, or an empty string if not found.
+ *
+ * @example
+ * ```typescript
+ * import { WinGetTitle } from '@ahmic/autoit-js';
+ *
+ * const title = await WinGetTitle('Untitled - Notepad');
+ *
+ * console.log(title); // Output: "Untitled - Notepad"
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinGetTitle.htm
+ */
+export async function WinGetTitle(
+  windowTitle: string,
+  windowText: string = '',
+  characterCount: number = 1024,
+): Promise<string> {
+  const [buffer, length] = createUnicodeBuffer(characterCount);
+
+  await autoit.invokeAsync(
     'AU3_WinGetTitle',
     VOID,
     [LPCWSTR, LPCWSTR, LPWSTR, INT],
