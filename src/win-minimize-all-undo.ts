d01678c85e16e3c029cd7dc376a8be129782f2bf
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 15:59:03 -0400

    feat: Implement async variant of WinMinimizeAllUndo
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinMinimizeAllUndo to WinMinimizeAllUndoSync
    
    #10

--- src/win-minimize-all-undo.ts
+++ src/win-minimize-all-undo.ts
@@ -6,13 +6,29 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinMinimizeAllUndo } from '@ahmic/autoit-js';
+ * import { WinMinimizeAllUndoSync } from '@ahmic/autoit-js';
  *
- * WinMinimizeAllUndo();
+ * WinMinimizeAllUndoSync();
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinMinimizeAllUndo.htm
  */
-export function WinMinimizeAllUndo(): void {
-  autoit.invoke('AU3_WinMinimizeAllUndo', VOID, [], []);
+export function WinMinimizeAllUndoSync(): void {
+  return autoit.invoke('AU3_WinMinimizeAllUndo', VOID, [], []);
+}
+
+/**
+ * Undoes the minimize all action, restoring all minimized windows.
+ *
+ * @example
+ * ```typescript
+ * import { WinMinimizeAllUndo } from '@ahmic/autoit-js';
+ *
+ * await WinMinimizeAllUndo();
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinMinimizeAllUndo.htm
+ */
+export function WinMinimizeAllUndo(): Promise<void> {
+  return autoit.invokeAsync('AU3_WinMinimizeAllUndo', VOID, [], []);
 }
