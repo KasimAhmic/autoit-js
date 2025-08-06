9fdb58573e1970d1c9607dbf6c0b464a5099dc4f
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 16:31:41 -0400

    feat: Implement async variant of WinSetTransByHandle
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinSetTransByHandle to WinSetTransByHandleSync
    
    #10

--- src/win-set-trans-by-handle.ts
+++ src/win-set-trans-by-handle.ts
@@ -11,15 +11,38 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinSetTransByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ * import { WinSetTransByHandleSync, WinGetHandleSync } from '@ahmic/autoit-js';
  *
- * const windowHandle = WinGetHandle('Untitled - Notepad');
+ * const windowHandle = WinGetHandleSync('Untitled - Notepad');
  *
- * WinSetTransByHandle(windowHandle, 128);
+ * WinSetTransByHandleSync(windowHandle, 128);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinSetTrans.htm
  */
-export function WinSetTransByHandle(windowHandle: bigint, transparency: number): number {
+export function WinSetTransByHandleSync(windowHandle: bigint, transparency: number): number {
   return autoit.invoke('AU3_WinSetTransByHandle', INT, [HWND, INT], [windowHandle, transparency]);
+}
+
+/**
+ * Sets the transparency level of a window. 0 is fully transparent, 255 is fully opaque.
+ *
+ * @param windowHandle The handle of the window.
+ * @param transparency The transparency level (0-255).
+ *
+ * @returns A promise that resolves to 1 if successful, or 0 otherwise.
+ *
+ * @example
+ * ```typescript
+ * import { WinSetTransByHandle, WinGetHandle } from '@ahmic/autoit-js';
+ *
+ * const windowHandle = await WinGetHandle('Untitled - Notepad');
+ *
+ * await WinSetTransByHandle(windowHandle, 128);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinSetTrans.htm
+ */
+export function WinSetTransByHandle(windowHandle: bigint, transparency: number): Promise<number> {
+  return autoit.invokeAsync('AU3_WinSetTransByHandle', INT, [HWND, INT], [windowHandle, transparency]);
 }
