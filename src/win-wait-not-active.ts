60b8eb99c6c7b9a971cf77c713309b6bdac5ada4
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-06 16:38:06 -0400

    feat: Implement async variant of WinWaitNotActive
    Part of the ongoing work of #10
    
    BREAKING CHANGE: Renamed WinWaitNotActive to WinWaitNotActiveSync
    
    #10

--- src/win-wait-not-active.ts
+++ src/win-wait-not-active.ts
@@ -12,15 +12,50 @@ import { autoit } from './lib/autoit';
  *
  * @example
  * ```typescript
- * import { WinWaitNotActive } from '@ahmic/autoit-js';
+ * import { WinWaitNotActiveSync } from '@ahmic/autoit-js';
  *
- * WinWaitNotActive('Untitled - Notepad', '', 10);
+ * WinWaitNotActiveSync('Untitled - Notepad', '', 10);
  * ```
  *
  * @see https://www.autoitscript.com/autoit3/docs/functions/WinWaitNotActive.htm
  */
-export function WinWaitNotActive(windowTitle: string, windowText: string = '', timeout: number = 0): number {
+export function WinWaitNotActiveSync(
+  windowTitle: string,
+  windowText: string = '',
+  timeout: number = 0,
+): number {
   return autoit.invoke(
+    'AU3_WinWaitNotActive',
+    INT,
+    [LPCWSTR, LPCWSTR, INT],
+    [windowTitle, windowText, timeout],
+  );
+}
+
+/**
+ * Waits for a window to become inactive.
+ *
+ * @param windowTitle The title of the window to wait for.
+ * @param windowText Optional text found in the window.
+ * @param timeout The timeout in seconds. Defaults to 0 (wait indefinitely).
+ *
+ * @returns A promise that resolves to 1 if the window becomes inactive, 0 if the timeout is reached.
+ *
+ * @example
+ * ```typescript
+ * import { WinWaitNotActive } from '@ahmic/autoit-js';
+ *
+ * await WinWaitNotActive('Untitled - Notepad', '', 10);
+ * ```
+ *
+ * @see https://www.autoitscript.com/autoit3/docs/functions/WinWaitNotActive.htm
+ */
+export function WinWaitNotActive(
+  windowTitle: string,
+  windowText: string = '',
+  timeout: number = 0,
+): Promise<number> {
+  return autoit.invokeAsync(
     'AU3_WinWaitNotActive',
     INT,
     [LPCWSTR, LPCWSTR, INT],
