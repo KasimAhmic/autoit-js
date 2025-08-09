c32fb8820b64761679370b8ee57ae98cea21417b
Kasim Ahmic <kasim.ahmic@gmail.com>
2025-08-09 17:23:42 -0400

    ci: Add JSDoc comments in the d.ts files
    The declaration file was having comments stripped out by tsc so I re-enabled that and enabled
    comment stripping in Rollup to prevent comment duplication

--- rollup.config.js
+++ rollup.config.js
@@ -13,6 +13,9 @@ const options = {
   external: ['node:os', 'node:path', 'node:url', 'node:util', 'koffi'],
   plugins: [
     typescript({
+      // Let tsc create the declaration files and leave the comments there. Strip it out in the JS code.
+      declaration: false,
+      removeComments: true,
       exclude: 'src/**/*.test.*',
     }),
   ],
